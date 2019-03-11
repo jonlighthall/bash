daa6e1383f4ee82a5f074d27684337671ddc914b
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-11 10:08:45 -0500

    update indentation

--- find_missing_and_empty.sh
+++ find_missing_and_empty.sh
@@ -11,41 +11,41 @@ if [ $# -eq 0 ]
 then
     echo "Please provide an input file"
 else
-if [ $FILE1 == missing.lst ]; then
-    cp missing.lst missing.bak
-    FILE1=missing.bak
-fi
-if [ -f missing.lst ]; then
-    rm missing.lst
-fi
-while read line; do
-    fpre="${line%.*}"
-    fname=$(printf '%s_02.asc' "$fpre" )
-    ((k++))
-#printf "%5d %s\n" $k $line
-    if [ ! -f $fname ]; then
-	((i++))
-    #echo $i $fname "is missing"
-	echo $line >> missing.lst
-    else
-	if [ ! -s $fname ]; then #adding empty increases runtime < 4%
-	    ((j++))
-      #echo $j $fname "is empty"
-	    echo $line >> missing.lst
-	fi
+    if [ $FILE1 == missing.lst ]; then
+	cp missing.lst missing.bak
+	FILE1=missing.bak
     fi
-done < $FILE1
-echo $k "filenames checked"
-echo $i "of" $k "files missing"
-((m=k-i))
-if [ $m == 0 ]; then
-    echo "no empty files found"
-else
-    echo $m "files found"
-    echo $j "of" $m "files empty"
-fi
-((l=i+j))
-echo $l "of" $k "problem files"
+    if [ -f missing.lst ]; then
+	rm missing.lst
+    fi
+    while read line; do
+	fpre="${line%.*}"
+	fname=$(printf '%s_02.asc' "$fpre" )
+	((k++))
+#printf "%5d %s\n" $k $line
+	if [ ! -f $fname ]; then
+	    ((i++))
+    #echo $i $fname "is missing"
+	    echo $line >> missing.lst
+	else
+	    if [ ! -s $fname ]; then #adding empty increases runtime < 4%
+		((j++))
+      #echo $j $fname "is empty"
+		echo $line >> missing.lst
+	    fi
+	fi
+    done < $FILE1
+    echo $k "filenames checked"
+    echo $i "of" $k "files missing"
+    ((m=k-i))
+    if [ $m == 0 ]; then
+	echo "no empty files found"
+    else
+	echo $m "files found"
+	echo $j "of" $m "files empty"
+    fi
+    ((l=i+j))
+    echo $l "of" $k "problem files"
 #if [ -f missing.lst ]; then
   #cat missing.lst
 #fi
