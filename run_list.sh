226bbc68ca47c7fdfe06c21e7a7e674867efb8cf
Jon Lighthall <jon.lighthall@gmail.com>
2019-03-18 10:35:13 -0500

    FIX: remove cd; add timing and cleanup

--- run_list.sh
+++ run_list.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 # reads a list of input files and if the file exists run the specified
 # program 
+
+#STARTTIME=$SECONDS 
 FILE1=$1
 PROGRAM=/home/jlighthall/nspe/nspe60_Dec2018/nspe.x
+#PROGRAM=/jfs02/iampsdata/commands/nspe58/nspe.x
+#PROGRAM=/jfs02/iampsdata/commands/nspe60/nspe.x
 
 if [ $# -eq 0 ]
 then
     echo "Please provide an input file"
 else
+/home/jlighthall/nspe/nspe60_Dec2018/testube/std_copy/clean_nspe.sh
 while read line; do
     printf "testing %s\n" $line
     if [ -f $line ]; then
-	echo $line "is dir"
-	cd $line
+	echo $line "exists"
 	 $PROGRAM $line
-	echo $line "is clean"
+	 echo $line "has been run"
+	#echo " " $(date) "at time $(($SECONDS - $STARTTIME))"
     else
-	echo $line " failed"
+	echo $line " not found"
     fi
 done < $FILE1
+#rm -rv /Acoustics/forjon/std/*
+#cp -rv /home/jlighthall/nspe/nspe60_Dec2018/testube/std_copy/. /Acoustics/forjon/std
 fi
+#echo " " $(date) "at time $(($SECONDS - $STARTTIME))"
+echo " " $(date) "at time $SECONDS"
+
