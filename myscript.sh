cef8973aaa1e8f69f8512c60a03f6d5676de4155
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-02 14:28:29 -0500

    add conditionals for stability

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-echo "First arg: $1"
-echo "Second arg: $2"
-cp $1 copy1
-cp $2 copy2
+if [ $# -eq 0 ]
+then
+    echo "Please provide two input files"
+else
+    echo "First arg: $1"
+    echo "Second arg: $2"
+    if [ ! -f $1 ]; then
+	echo "file $1 does not exist. creating..."
+	touch $1
+    fi
+    cp -v $1 copy1
+    if [ ! -f $2 ]; then
+	echo "file $2 does not exist. creating..."
+	touch $2
+    fi
+    cp -v $2 copy2
+fi
