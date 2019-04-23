4e552f600305e7c893f50f4266c600597aa0680d
Jon Lighthall <jon.lighthall@gmail.com>
2019-04-23 09:53:32 -0500

    move copy to prompt conditional

--- userinput.sh
+++ userinput.sh
@@ -9,19 +9,18 @@ else
 	echo "file $1 does not exist. creating..."
 	touch $1
     fi
-
-    read -p "Proceed with copy? (y/n) " -n 1 -r
-
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-	echo 'yes'
-    else
-	echo 'no'
-    fi
-    cp -v $1 copy1
     if [ ! -f $2 ]; then
 	echo "file $2 does not exist. creating..."
 	touch $2
     fi
-    cp -v $2 copy2
+    read -p "Proceed with copy? (y/n) " -n 1 -r
+
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+	echo -e '\ncopying...'
+	cp -v $1 copy1
+	cp -v $2 copy2
+    else
+	echo -e '\nexiting...'
+    fi
 fi
