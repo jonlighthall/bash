e72fb709d41125fc0c4bf7920ccfa08dcd566fc2
Jon Lighthall <jon.lighthall@gmail.com>
2019-12-09 09:49:45 -0600

    update excludes in reset arp from nov 19

--- reset_arp.sh
+++ reset_arp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -r ens*
-find -type f -not -name "reset.sh" -not -name "*.nc" -not -name "*.dat" -exec rm {} \;
+find -type f -not -path "./.git/*" -not -name ".git*" -not -name "*.nc" -not -name "*.dat" -exec rm {} \;
