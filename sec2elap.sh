e10f304b955df1099a46c5fa8a5600f1fc47f488
Jon Lighthall <jon.lighthall@gmail.com>
2019-12-18 15:21:40 -0600

    add less than min
    remove date
    fix hour comparison
    add handling for less than one minute

--- sec2elap.sh
+++ sec2elap.sh
@@ -3,17 +3,19 @@ if [ $# -eq 0 ]
 then
     echo "Please provide an input"
 else
-    echo $(date) "at end, elapsed time $1 sec"
-    if [[ $1 < $((60*60)) ]]; then
-	echo $(date -d @${1} +"%M min %S sec")
-    else
-	HR=$(($1/(60*60)))
-	if (( $HR < 24 )); then
-	    echo $(date -d @${1} +"$HR hours %M min %S sec")
+    echo "Elapsed time is $1 sec"
+    if (( $1 >= 60 )); then
+	if (( $1 < $((60*60)) )); then
+	    echo $(date -d @${1} +"%M min %S sec")
 	else
-	    DY=$(($HR/24)) 
-	    HR=$(($HR - $DY*24))
-	    echo $(date -d @${1} +"$DY days $HR hours %M min %S sec")
+	    HR=$(($1/(60*60)))
+	    if (( $HR < 24 )); then
+		echo $(date -d @${1} +"$HR hours %M min %S sec")
+	    else
+		DY=$(($HR/24)) 
+		HR=$(($HR - $DY*24))
+		echo $(date -d @${1} +"$DY days $HR hours %M min %S sec")
+	    fi
 	fi
     fi
 fi
