39087576a15e036d11af5c899748161c76907815
Jon Lighthall <jon.lighthall@gmail.com>
2020-02-28 10:38:53 -0600

    xtext add close programs

--- xtest.sh
+++ xtest.sh
@@ -2,9 +2,18 @@
 
 # test X11
 export LC_ALL=C
-xeyes &
-xclock &
-xcalc &
-xman &
-xlogo &
-xterm &
+xeyes 2>/dev/null &
+xclock 2>/dev/null &
+xcalc 2>/dev/null &
+xman 2>/dev/null &
+xlogo 2>/dev/null &
+xterm 2>/dev/null &
+
+read -n 1 -s -r -p "Press any key to continue"
+echo
+pkill xeyes 
+pkill xclock
+pkill xcalc
+pkill xman
+pkill xlogo
+pkill xterm
