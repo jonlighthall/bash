37594057c28847a421d4b979507ec8f3c08d6e66
Jon Lighthall <jon.lighthall@gmail.com>
2020-07-10 14:45:26 -0500

    add second argument for program

--- run_list.sh
+++ run_list.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-# reads a list of input files and if the file exists run the specified
-# program 
-
-FILE1=$1
-PROGRAM=/home/jlighthall/nspe/nspe60_Dec2018/nspe.x
-#PROGRAM=/jfs02/iampsdata/commands/nspe58/nspe.x
-#PROGRAM=/jfs02/iampsdata/commands/nspe60/nspe.x
+# Reads a list of input files and, if the list file exists, run the
+# specified program. The first arugement is rqpured and specifies the
+# run list file. An optional second argument can be used to specifed
+# the program.
 
 if [ $# -eq 0 ]
 then
     echo "Please provide an input file"
 else
+    FILE1=$1
+    if [ $# -eq 2 ]; then
+	PROGRAM=$2
+    else
+	PROGRAM=/home/jlighthall/nspe/nspe60_Dec2018/nspe.x
+	#PROGRAM=/jfs02/iampsdata/commands/nspe58/nspe.x
+	#PROGRAM=/jfs02/iampsdata/commands/nspe60/nspe.x
+    fi
+    echo Program is $PROGRAM
     while read line; do
 	printf "testing %s..." $line
 	if [ -f $line ]; then
