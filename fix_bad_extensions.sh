57b4762587bb8eb75328d00a6b244d788412577c
Jon Lighthall <jon.lighthall@gmail.com>
2021-11-02 13:36:55 -0500

    add find in loop

--- fix_bad_extensions.sh
+++ fix_bad_extensions.sh
@@ -1,8 +1,13 @@
 # used to un-fix bad file extensions for OneDrive
+# JCL Nov 2021
+
+# define replacement seperator
 sep=_._
+
 for bad in out exe bat
 do
-    for fname in *.$bad; do
+    echo "replacing \".$bad\" with \"${sep}${bad}\"..."
+    for fname in $(find $PWD -name "*.${bad}"); do
 	mv -nv "$fname" "`echo $fname | sed "s/.$bad/$sep$bad/"`";
     done
 done
