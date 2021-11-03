3b12e754e9534100e794ce48d04068fdcef20607
Jon Lighthall <jon.lighthall@gmail.com>
2021-11-03 15:09:46 -0500

    REMOVE colons, reorg

--- rmbin.sh
+++ rmbin.sh
@@ -1,6 +1,6 @@
 # Used to delete binary files from directory. Tracked binary files
 # found in a Git repository will be removed from the repository. The
-# .git directory will not be searched JCL Nov 2021
+# .git directory will not be searched. JCL Nov 2021
 
 if [ $# -eq 0 ]
 then
@@ -14,11 +14,9 @@ else
 	echo $PWD
 
 	if git rev-parse --git-dir > /dev/null 2>&1; then
-	    : # This is a valid git repository (but the current working
-#	    git rev-parse --git-dir
-#	    echo $(git rev-parse --git-dir)
-	    GITDIR=$(git rev-parse --git-dir)
+	    # This is a valid git repository
 	    echo "$1 is part of a Git repository"
+	    GITDIR=$(git rev-parse --git-dir)
 	    echo "the .git folder is $GITDIR"
 
 	    # first, remove tracked files from the repository
@@ -28,10 +26,10 @@ else
 	    # then, remove remaining binary files
 	    echo "removing untracked binary files..."
 	    find ./ -type f -not -path "*$GITDIR/*" | perl -lne 'print if -B' | xargs -r ls
-    
 	else
-	    : # this is not a git repository
+	    # this is not a git repository
 	    echo "$1 is not part of a Git repsoity"
+	    echo "removing binary files..."
 	    find ./ -type f | perl -lne 'print if -B' | xargs -r ls	    
 	fi
     else
