1fda13d94b22a5655fbfa464057b085d723787c4
Jon Lighthall <jon.lighthall@gmail.com>
2021-11-03 15:01:09 -0500

    FIX launch location and add progs

--- make_links.sh
+++ make_links.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir -pv ~/bin
 # list files to be linked in bin
-for prog in bell sec2elap whatsup rmbin
+for prog in bell sec2elap whatsup rmbin fix_bad_extensions unfix_bad_extensions test_file
 do
     if [ ! -f $HOME/bin/$prog ]; then
-	ln -sv $HOME/bash/$prog.sh $HOME/bin/$prog
+	ln -svf $PWD/$prog.sh $HOME/bin/$prog
     fi
 done
