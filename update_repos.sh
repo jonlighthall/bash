d30d57801c872bf6bbf2eaa3bdd38e9df95bab40
Jon Lighthall <jon.lighthall@gmail.com>
2022-05-02 11:23:43 -0500

    add push/pull all and add hlines

--- update_repos.sh
+++ update_repos.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 echo "${0##*/}"
 
+# deinfe horizontal line
+hline() {
+    for i in {1..64}; do echo -n "-"; done
+    echo
+}
+
 # list repository paths, relative to home
 list="bash batch config fortran hello matlab/macros nrf powershell scripts"
 
 for repo in $list
 do
+    hline
     echo -e "updating $repo... \c"
     if [ -d ${HOME}/$repo ]; then
 	echo "OK"
 	cd ${HOME}/$repo
-	git pull
-	git push    
+	git pull --all
+	git push --all
     else
 	echo "not found"
     fi
+    hline
+    echo
 done
