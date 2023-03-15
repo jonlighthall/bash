e321704f56f9580c33da872db435e2bb4dc80eb6
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-15 10:33:55 -0500

    add testing to make links

--- make_links.sh
+++ make_links.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
-mkdir -pv ~/bin
+TAB="   "
+
+# set source and target directories
+SRCDIR=$PWD
+TGTDIR=$HOME/bin
+
+# check target directory
+echo -n "target directory $TGTDIR... "
+if [ -d $TGTDIR ]; then
+    echo "exists"
+else
+    echo "does not exist"
+    mkdir -pv $TGTDIR
+fi
+
 # list files to be linked in bin
 for prog in bell sec2elap whatsup rmbin fix_bad_extensions \
     unfix_bad_extensions test_file update_repos update_packages \
     untar clean_mac add_path log ls_test
 do
-    if [ ! -f $HOME/bin/$prog ]; then
-	if [ -f $PWD/$prog.sh ]; then
-	    ln -svf $PWD/$prog.sh $HOME/bin/$prog
+    echo -n "program $SRCDIR/$prog.sh... "
+    if [ -e $SRCDIR/$prog.sh ]; then
+	echo -n "exists and is "
+	if [ -x $SRCDIR/$prog.sh ]; then
+	    echo "executable"
+	    echo -n "${TAB}link $TGTDIR/${prog}... "
+	    if [ -e $TGTDIR/${prog} ] ; then
+		echo -n "exists and "
+		if [[ $SRCDIR/$prog.sh -ef $TGTDIR/$prog ]]; then
+		    echo "already points to ${prog}. skipping..."
+		    continue
+		else
+		    echo -n "will be backed up..."
+		    mv -v $TGTDIR/${prog} $TGTDIR/${prog}_$(date +'%Y-%m-%d-t%H%M')
+		fi
+	    else
+		echo "does not exist"
+	    fi
+	    echo -n "${TAB}making link... "
+	    ln -svf $SRCDIR/$prog.sh $TGTDIR/$prog
+	else
+	    echo "not executable"
 	fi
+    else
+	echo "does not exist"
     fi
 done
