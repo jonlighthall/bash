ab22f622c50a3c41dc43357b84a46f0e7667e253
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-22 13:41:19 -0500

    add what's up mods for snuffy

--- whatsup.sh
+++ whatsup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
 echo "   host:" $HOSTNAME
 echo -n "     IP: ";hostname -I
 echo "   user:" $USER$USERNAME
 echo " groups:" `id -nG`
 echo "    pwd:" $PWD
 echo "   date:" $(date)
-echo "    PID:  $PPID"
-echo "   time:" $(sec2elap $(ps -p $PPID -o etimes | tail -n 1))
+echo "    PID: $PPID"
+echo -n "   time:"
+if (ps -o etimes) > /dev/null 2>&1; then
+    echo "yes"
+    echo $(sec2elap $(ps -p "$PPID" -o etimes | tail -n 1))
+else
+    echo $(ps -p $PPID -o etime)
+fi
