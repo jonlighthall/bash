162ba6be479e03ebdd42393c0d630ad06ea7f5f0
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-22 15:44:44 -0400

    add link and target variables, unify with fortran utils

--- make_links.sh
+++ make_links.sh
@@ -1,51 +1,58 @@
 #!/bin/bash
+echo $BASH_SOURCE
 TAB="   "
 
 # set source and target directories
-SRCDIR=$PWD
-TGTDIR=$HOME/bin
+source_dir=$PWD
+user_bin=$HOME/bin
 
 # check target directory
-echo -n "target directory $TGTDIR... "
-if [ -d $TGTDIR ]; then
+echo -n "target directory $user_bin... "
+if [ -d $user_bin ]; then
     echo "exists"
 else
     echo "does not exist"
-    mkdir -pv $TGTDIR
+    mkdir -pv $user_bin
 fi
 
-echo "source directory $SRCDIR"
+echo "source directory $source_dir"
 
 echo "--------------------------------------"
 echo "------ Start Linking Repo Files-------"
 echo "--------------------------------------"
 
 # list files to be linked
+ext=.sh
 for prog in bell sec2elap whatsup rmbin fix_bad_extensions \
 		 unfix_bad_extensions test_file update_repos update_packages \
 		 untar clean_mac add_path log ls_test
 do
-    echo -n "program $SRCDIR/$prog.sh... "
-    if [ -e $SRCDIR/$prog.sh ]; then
+    target=${source_dir}/${prog}${ext}
+    link=${user_bin}/$prog
+
+    echo -n "program $target... "
+    if [ -e $target ]; then
 	echo -n "exists and is "
-	if [ -x $SRCDIR/$prog.sh ]; then
+	if [ -x $target ]; then
 	    echo "executable"
-	    echo -n "${TAB}link $TGTDIR/${prog}... "
-	    if [ -e $TGTDIR/${prog} ] || [ -L $TGTDIR/${prog} ] || [ -d $TGTDIR/${prog} ] ; then
+	    echo -n "${TAB}link $link... "
+	    if [ -e $link ] || [ -L $link ] || [ -d $link ] ; then
 		echo -n "exists and "
-		if [[ $SRCDIR/$prog.sh -ef $TGTDIR/$prog ]]; then
+		if [[ $target -ef $link ]]; then
 		    echo "already points to ${prog}"
+		    echo -n "${TAB}"
+		    ls -lhG --color=auto $link
 		    echo "${TAB}skipping..."
 		    continue
 		else
 		    echo -n "will be backed up..."
-		    mv -v $TGTDIR/${prog} $TGTDIR/${prog}_$(date +'%Y-%m-%d-t%H%M')
+		    mv -v $link ${link}_$(date +'%Y-%m-%d-t%H%M')
 		fi
 	    else
 		echo "does not exist"
 	    fi
 	    echo -n "${TAB}making link... "
-	    ln -sv $SRCDIR/$prog.sh $TGTDIR/$prog
+	    ln -sv $target $link
 	else
 	    echo "not executable"
 	fi
