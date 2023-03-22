24b3224a0c9bb57217a1bb2d23a59daaff13faf1
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-22 14:42:58 -0400

    removed debug print from whatsup

--- whatsup.sh
+++ whatsup.sh
@@ -8,8 +8,7 @@ echo "   date:" $(date)
 echo "    PID: $PPID"
 echo -n "   time:"
 if (ps -o etimes) > /dev/null 2>&1; then
-    echo "yes"
-    echo $(sec2elap $(ps -p "$PPID" -o etimes | tail -n 1))
+    echo " $(sec2elap $(ps -p "$PPID" -o etimes | tail -n 1))"
 else
     echo $(ps -p $PPID -o etime)
 fi
