d588f37af0a08450e3f329681ca405624b18e52b
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-22 14:07:45 -0400

    add PID and elapsed to what's up

--- whatsup.sh
+++ whatsup.sh
@@ -6,3 +6,5 @@ echo "   user:" $USER$USERNAME
 echo " groups:" `id -nG`
 echo "    pwd:" $PWD
 echo "   date:" $(date)
+echo "    PID:  $PPID"
+echo "   time:" $(sec2elap $(ps -p $PPID -o etimes | tail -n 1))
