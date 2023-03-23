370fec2689a8f2727b7f76564c83f0456ee5d702
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-23 13:25:46 -0500

    add undel repo

---
+++ undel_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Checks a Git repository for deleted files and restores those files
+# by checking them out
+
+echo $BASH_SOURCE
+
+# get list of deleted files
+list=$(git ls-files -d)
+
+# checkout deleted files
+for fname in $list
+do
+    echo $fname
+    git checkout $fname
+done
+wait 
+echo "${TAB}$(date): ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
