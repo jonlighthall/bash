f4ea73f44b3ce74c127c6e12300d8ee34c5ee27d
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-31 16:34:20 -0500

    add client path and fix time fmt

--- whatsup.sh
+++ whatsup.sh
@@ -8,9 +8,10 @@ echo " groups:" `id -nG`
 echo "    pwd:" $PWD
 echo "   date:" $(date)
 echo "    PID: $PPID"
-echo -n "   time:"
+echo -n "   time: "
 if (ps -o etimes) > /dev/null 2>&1; then
-    echo " $(sec2elap $(ps -p "$PPID" -o etimes | tail -n 1))"
+    echo "$(sec2elap $(ps -p "$PPID" -o etimes | tail -n 1))"
 else
     echo $(ps -p $PPID -o etime)
 fi
+echo "   path: $USER$USERNAME@$HOSTNAME:$PWD"
