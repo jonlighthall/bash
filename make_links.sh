c15215efb585f573c6451c834090b2257c859608
Jon Lighthall <jon.lighthall@gmail.com>
2023-03-31 21:55:34 -0500

    format and unify with scripts make links

--- make_links.sh
+++ make_links.sh
@@ -29,45 +29,61 @@ echo "--------------------------------------"
 
 # list files to be linked
 ext=.sh
-for prog in add_path bell clean_mac fix_bad_extensions log ls_test merge_history rmbin sec2elap \
-		 test_file undel_repo unfix_bad_extensions untar update_packages update_repos \
-		 whatsup xtest find_missing_and_empty
+for prog in \
+    add_path \
+    bell \
+    clean_mac \
+    find_missing_and_empty \
+    fix_bad_extensions \
+    log \
+    ls_test \
+    merge_history \
+    rmbin \
+    sec2elap \
+    test_file \
+    undel_repo \
+    unfix_bad_extensions \
+    untar \
+    update_packages \
+    update_repos \
+    whatsup \
+    xtest \
+
 do
     target=${source_dir}/${prog}${ext}
     link=${user_bin}/${prog}
 
     echo -n "program $target... "
     if [ -e $target ]; then
-	echo -n "exists and is "
-	if [ -x $target ]; then
-	    echo "executable"
-	    echo -n "${TAB}link $link... "
-	    # first, backup existing copy
-	    if [ -L $link ] || [ -f $link ] || [ -d $link ]; then
-		echo -n "exists and "
-		if [[ $target -ef $link ]]; then
-		    echo "already points to ${prog}"
-		    echo -n "${TAB}"
-		    ls -lhG --color=auto $link
-		    echo "${TAB}skipping..."
-		    continue
-		else
-		    echo -n "will be backed up..."
-		    mv -v $link ${link}_$(date +'%Y-%m-%d-t%H%M')
-		fi
-	    else
-		echo "does not exist"
-	    fi
-	    # then link
-	    echo -n "${TAB}making link... "
-	    ln -sv $target $link
-	else
-	    echo "not executable"
-	fi
+        echo -n "exists and is "
+        if [ -x $target ]; then
+            echo "executable"
+            echo -n "${TAB}link $link... "
+            # first, backup existing copy
+            if [ -L $link ] || [ -f $link ] || [ -d $link ]; then
+                echo -n "exists and "
+                if [[ $target -ef $link ]]; then
+                    echo "already points to ${prog}"
+                    echo -n "${TAB}"
+                    ls -lhG --color=auto $link
+                    echo "${TAB}skipping..."
+                    continue
+                else
+                    echo -n "will be backed up..."
+                    mv -v $link ${link}_$(date +'%Y-%m-%d-t%H%M')
+                fi
+            else
+                echo "does not exist"
+            fi
+            # then link
+            echo -n "${TAB}making link... "
+            ln -sv $target $link
+        else
+            echo "not executable"
+        fi
     else
-	echo "does not exist"
+        echo "does not exist"
     fi
-    echo
 done
 echo "--------------------------------------"
 echo "--------- Done Making Links ----------"
