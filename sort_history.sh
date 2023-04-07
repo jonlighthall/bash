92e4f5cc1032246b5ace1ade8d279756ebc3f005
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-07 16:01:08 -0500

    add tabs to print sort hist

--- sort_history.sh
+++ sort_history.sh
@@ -5,6 +5,8 @@
 #
 # JCL Apr 2023
 
+export TAB="   "
+
 # check for reference file
 hist_in=${HOME}/.bash_history
 hist_out=${hist_in}_edit
@@ -17,38 +19,41 @@ else
 fi
 
 # copy file
-echo "copying..."
-cp -pv ${hist_in} ${hist_out}
+echo "${TAB}copying..."
+cp -pv ${hist_in} ${hist_out} | sed "s/^/${TAB}${TAB}/"
 
 # delete blank lines
-echo "delete blank lines..."
+echo "${TAB}delete blank lines..."
 sed -i 's/^$//' ${hist_out}
 \diff --suppress-common-lines ${hist_in} ${hist_out}
 
 # find and mark timestamp lines
-echo "mark timestamp lines..."
+echo "${TAB}mark timestamp lines..."
 sed -i 's/^#[0-9]\{10\}.*/&$$$/' ${hist_out}
 
 # remove marks from timestamp lines with no associated commands
-echo "un-mark childless timestamp lines..."
+echo "${TAB}un-mark childless timestamp lines..."
 sed -i ':start;N;s/\$\$\$\n#/\n#/;t start;P;D' ${hist_out}
 
 # merge commands with timestamps
-echo "merge commands with timestamp lines..."
+echo "${TAB}merge commands with timestamp lines..."
 sed -i ':start;N;s/\$\$\$\n/$$$/;t start;P;D' ${hist_out}
 
 # mark orphaned lines
-echo "mark orphaned lines..."
+echo "${TAB}mark orphaned lines..."
 sed -i 's/^[^#]/@@@&/' ${hist_out}
 
 # merge commands with timestamps
-echo "merge orphaned lines..."
+echo "${TAB}merge orphaned lines..."
 sed -i ':start;N;s/\n@@@/@@@/;t start;P;D' ${hist_out}
 
 # sort history
-echo "sorting lines..."
+echo "${TAB}sorting lines..."
 sort -u ${hist_out} -o ${hist_out}
 
 # unmerge commands
-echo "unmerge commands..."
+echo "${TAB}unmerge commands..."
 sed -i 's/\$\$\$/\n/;s/@@@/\n/' ${hist_out}
+
+# print time at exit
+echo -e "\n$(date +"%R") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
