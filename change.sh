cc359b7bb5df1747b8a8283ec78a206a8e878667
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-24 09:23:58 -0500

    change old to wrong in change.sh

--- change.sh
+++ change.sh
@@ -2,15 +2,15 @@
 
 git filter-branch --commit-filter '
 
-OLD_EMAIL="lighthall@2b1ce1e5-f9e2-4de2-b566-7fb11778057d"
+WRONG_EMAIL="lighthall@2b1ce1e5-f9e2-4de2-b566-7fb11778057d"
 CORRECT_NAME="Jon Lighthall"
 CORRECT_EMAIL="jon.lighthall@gmail.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]; then
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]; then
     GIT_COMMITTER_NAME="$CORRECT_NAME"
     GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]; then
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]; then
     GIT_AUTHOR_NAME="$CORRECT_NAME"
     GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
