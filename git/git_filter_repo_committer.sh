7a7fdb9d5b92035a1fe81ae900782d815bea7187
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-26 22:23:50 -0500

    add partial

--- git/git_filter_repo_committer.sh
+++ git/git_filter_repo_committer.sh
@@ -1,4 +1,4 @@
-git filter-repo --commit-callback '
+git filter-repo --partial --commit-callback '
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
     correct_email = b"jon.lighthall@gmail.com"
     if commit.committer_email in auth_list:
