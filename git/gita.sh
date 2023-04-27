741625a33d537b9fabdc2c6a9b892d6a434d728a
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 15:45:55 -0500

    add uniq and fix typo

--- git/gita.sh
+++ git/gita.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 TAB="     "
 echo "current author name:"
-git config --get-all user.name | sed "s/^/${TAB}/"
+git config --get-all user.name | uniq | sed "s/^/${TAB}/"
 echo
 echo "current author email:"
-git config --get-all user.email | sed "s/^/${TAB}/"
+git config --get-all user.email | uniq | sed "s/^/${TAB}/"
 echo
 if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
     echo "list of authors:"
     git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     echo
     echo "list of names:"
-    git log --pretty=format:"%aN%" | sort -u | sed "s/^/${TAB}/"
+    git log --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
     echo
     echo "list of emails:"
-    git log --pretty=format:"%aE%" | sort -u | sed "s/^/${TAB}/"
+    git log --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
 else
     echo "$PWD is not a Git repsoitory"
 fi
