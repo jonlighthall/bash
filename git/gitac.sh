e178ab6e0e2b3a5d39f8a2166de01f03a70b666b
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 15:42:10 -0500

    remove commiters and taggers from gita

--- git/gitac.sh
+++ git/gitac.sh
@@ -10,11 +10,14 @@ if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
     echo "list of authors:"
     git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     echo
+    echo "list of committers:"
+    git log --pretty=format:"%cN %cE" | sort | uniq -c | sort -n
+    echo
     echo "list of names:"
-    git log --pretty=format:"%aN%" | sort -u | sed "s/^/${TAB}/"
+    git log --pretty=format:"%aN%n%cN" | sort -u | sed "s/^/${TAB}/"
     echo
     echo "list of emails:"
-    git log --pretty=format:"%aE%" | sort -u | sed "s/^/${TAB}/"
+    git log --pretty=format:"%aE%n%cE" | sort -u | sed "s/^/${TAB}/"
 else
     echo "$PWD is not a Git repsoitory"
 fi
