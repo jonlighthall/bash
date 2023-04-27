572128e29b7c4b001cc3f4246645d79b03359e1c
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 14:08:57 -0500

    pull formatting from cui config make links

--- make_links.sh
+++ make_links.sh
@@ -87,10 +87,10 @@ do
                 echo "does not exist"
             fi
             # then link
-	    echo -n "${TAB}";hline 72;
-	    echo -n "${TAB}making link... "
-	    ln -sv $target $link
-	    echo -n "${TAB}";hline 72;
+	    echo -en "${TAB}${GRH}";hline 72;
+	    echo "${TAB}making link... "
+	    ln -sv $target $link | sed "s/^/${TAB}/"
+	    echo -ne "${TAB}";hline 72;echo -en "${NORMAL}"
         else
             echo -e "${BAD}not executable${NORMAL}"
         fi
