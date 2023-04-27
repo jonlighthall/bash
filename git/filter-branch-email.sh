91ac6894eb6c09b71917da324cb514f49d49433f
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 13:16:46 -0500

    add append to filter branch scripts

--- git/filter-branch-email.sh
+++ git/filter-branch-email.sh
@@ -5,6 +5,7 @@ rm -rdv ./.git-rewrite
 git filter-branch $1 --env-filter '
 WRONG_EMAILS="lighthall@lsu.eud \
               jlighthall@fsu.edu"
+WRONG_EMAIL+=" jonathan.lighthall@"
 CORRECT_NAME="Jon Lighthall"
 CORRECT_EMAIL="jon.lighthall@gmail.com"
 
