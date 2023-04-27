d5637303e12d319824310e9b83752f5b3f4c7bfd
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 13:15:03 -0500

    add append to filter repo scripts

--- git/filter-repo-author.sh
+++ git/filter-repo-author.sh
@@ -1,6 +1,7 @@
 git filter-repo $1 --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
+    auth_list.append(b"jonathan.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
     if commit.author_email in auth_list:
         commit.author_email = correct_email
--- git/filter-repo-committer.sh
+++ git/filter-repo-committer.sh
@@ -1,6 +1,7 @@
 git filter-repo $1 --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
+    auth_list.append(b"jonathan.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
     if commit.committer_email in auth_list:
         commit.committer_email = correct_email
