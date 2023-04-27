955d6cd211709273e2b9456e9ff6605b48c65608
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-27 13:31:05 -0500

    add test for git repsoity

--- git/gita.sh
+++ git/gita.sh
@@ -6,18 +6,22 @@ echo
 echo "current author email:"
 git config --get-all user.email | sed "s/^/${TAB}/"
 echo
-echo "list of authors:"
-git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
-echo
-echo "list of committers:"
-git log --pretty=format:"%cN %cE" | sort | uniq -c | sort -n
-echo
-echo "list of taggers:"
-git tag --format="%(authorname) %(authoremail)" | sort -u | sed "s/^/${TAB}/"
-echo
-echo "list of names:"
-git log --pretty=format:"%aN%n%cN" | sort -u | sed "s/^/${TAB}/"
-echo
-echo "list of emails:"
-git log --pretty=format:"%aE%n%cE" | sort -u | sed "s/^/${TAB}/"
+if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+    echo "list of authors:"
+    git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+    echo
+    echo "list of committers:"
+    git log --pretty=format:"%cN %cE" | sort | uniq -c | sort -n
+    echo
+    echo "list of taggers:"
+    git tag --format="%(authorname) %(authoremail)" | sort -u | sed "s/^/${TAB}/"
+    echo
+    echo "list of names:"
+    git log --pretty=format:"%aN%n%cN" | sort -u | sed "s/^/${TAB}/"
+    echo
+    echo "list of emails:"
+    git log --pretty=format:"%aE%n%cE" | sort -u | sed "s/^/${TAB}/"
+else
+    echo "$PWD is not a Git repsoitory"
+fi
 echo
