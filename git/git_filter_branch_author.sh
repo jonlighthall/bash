9eea720b06b4207030f34744d7cd37cdb8ddd441
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-26 20:16:28 -0500

    add individual git filter branch scripts

---
+++ git/git_filter_branch_author.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export FILTER_BRANCH_SQUELCH_WARNING=1
+rm -rdv ./.git-rewrite
+
+git filter-branch --env-filter '
+WRONG_EMAILS="lighthall@lsu.eud \
+              jlighthall@fsu.edu"
+CORRECT_NAME="Jon Lighthall"
+CORRECT_EMAIL="jon.lighthall@gmail.com"
+
+for EMAIL in $WRONG_EMAILS
+do
+    if [ "$GIT_AUTHOR_EMAIL" = "$EMAIL" ]; then
+        GIT_AUTHOR_NAME="$CORRECT_NAME"
+        GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    fi
+done
+' --tag-name-filter cat -- --branches --tags
---
+++ git/git_filter_branch_committer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export FILTER_BRANCH_SQUELCH_WARNING=1
+rm -rdv ./.git-rewrite
+
+git filter-branch --env-filter '
+WRONG_EMAILS="lighthall@lsu.eud \
+              jlighthall@fsu.edu"
+CORRECT_NAME="Jon Lighthall"
+CORRECT_EMAIL="jon.lighthall@gmail.com"
+
+for EMAIL in $WRONG_EMAILS
+do
+    if [ "$GIT_COMMITTER_EMAIL" = "$EMAIL" ]; then
+        GIT_COMMITTER_NAME="$CORRECT_NAME"
+        GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    fi
+done
+' --tag-name-filter cat -- --branches --tags
