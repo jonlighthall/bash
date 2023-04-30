19414322372fdfee990627020e3990b88aab611c
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-30 11:44:12 -0500

    remove print

--- wiggler.sh
+++ wiggler.sh
@@ -4,8 +4,6 @@ start=$((-1*step))
 while [ .true ]; do
     dx=$(($RANDOM % range + start))
     dy=$(($RANDOM % range + start))
-    echo "($dx,$dy)"
     xdotool mousemove_relative -- dx dy
     sleep 1s
 done
-trap "echo ' $(sec2elap $SECONDS)'" EXIT
