78f7edddd9348608e8d1df8925468a0299295e31
Jon Lighthall <jon.lighthall@gmail.com>
2023-04-30 16:06:37 -0500

    add additional email to auth list, fix indentation in filter repo email

--- git/filter-repo-author.sh
+++ git/filter-repo-author.sh
@@ -2,6 +2,7 @@ git filter-repo $1 --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
     auth_list.append(b"jonathan.lighthall@")
+    auth_list.append(b"jonathan.c.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
     if commit.author_email in auth_list:
         commit.author_email = correct_email
--- git/filter-repo-email.sh
+++ git/filter-repo-email.sh
@@ -2,9 +2,10 @@ git filter-repo $1 --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
     auth_list.append(b"jonathan.lighthall@")
+    auth_list.append(b"jonathan.c.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
     if commit.author_email in auth_list:
-       commit.author_email = correct_email
+        commit.author_email = correct_email
         if commit.author_name != correct_name:
             commit.author_name = correct_name
     if commit.committer_email in auth_list:
@@ -15,4 +16,4 @@ git filter-repo $1 --partial --commit-callback '
        commit.tagger_email = correct_email
         if commit.tagger_name != correct_name:
             commit.tagger_name = correct_name
-  '
+'
