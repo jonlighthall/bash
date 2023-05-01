5d63ae3734657b08a575800bcfadef44e6bc715d
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-01 09:20:08 -0500

    add users.noreply.github

--- git/filter-repo-email.sh
+++ git/filter-repo-email.sh
@@ -1,6 +1,7 @@
 git filter-repo $@ --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
+    auth_list.append(b"jonlighthall@users.noreply.github.com")
     auth_list.append(b"jonathan.lighthall@")
     auth_list.append(b"jonathan.c.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
@@ -9,11 +10,11 @@ git filter-repo $@ --partial --commit-callback '
         if commit.author_name != correct_name:
             commit.author_name = correct_name
     if commit.committer_email in auth_list:
-       commit.committer_email = correct_email
+        commit.committer_email = correct_email
         if commit.committer_name != correct_name:
             commit.committer_name = correct_name
     if commit.tagger_email in auth_list:
-       commit.tagger_email = correct_email
+        commit.tagger_email = correct_email
         if commit.tagger_name != correct_name:
             commit.tagger_name = correct_name
 '
