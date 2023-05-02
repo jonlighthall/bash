02c1c3fa99285453cb2f43de93cd586236ff19a4
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-02 15:51:38 -0500

    add bad email to list

--- git/filter-repo-author.sh
+++ git/filter-repo-author.sh
@@ -2,6 +2,7 @@ git filter-repo $@ --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
     auth_list.append(b"jonlighthall@users.noreply.github.com")
+    auth_list.append(b"jon.lighthall@ygmail.com")
     auth_list.append(b"jonathan.lighthall@")
     auth_list.append(b"jonathan.c.lighthall@")
     correct_email = b"jon.lighthall@gmail.com"
