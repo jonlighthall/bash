3a851bf94385457a26dce37b1cc820912ff7cfe3
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-03 15:59:41 -0500

    add log all to gita

--- git/gita.sh
+++ git/gita.sh
@@ -8,13 +8,13 @@ git config --get-all user.email | uniq | sed "s/^/${TAB}/"
 echo
 if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
     echo "list of authors:"
-    git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+    git log --all --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     echo
     echo "list of names:"
-    git log --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
+    git log --all --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
     echo
     echo "list of emails:"
-    git log --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
+    git log --all --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
 else
     echo "$PWD is not a Git repsoitory"
 fi
