8129709be71604bf21fbf925b7334407929a16ec
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-03 16:02:17 -0500

    add automatic branch determination to filter repo author

--- git/filter-repo-author.sh
+++ git/filter-repo-author.sh
@@ -1,4 +1,22 @@
-git filter-repo $@ --partial --commit-callback '
+set -e
+# parse remote
+if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
+    echo "no remote tracking branch"
+else
+    branch_tracking=$(git branch -vv | grep \* | sed 's/^.*\[//;s/\(]\|:\).*$//')
+    echo -e "remote tracking branch is \033[34m${branch_tracking}\033[m"
+    name_remote=${branch_tracking%%/*}
+    echo "remote is name $name_remote"
+    url_remote=$(git remote -v | grep ${name_remote} |  awk '{print $2}' | sort -u)
+    echo "remote url is ${url_remote}"
+    # parse branches
+    branch_remote=${branch_tracking#*/}
+    echo "remote branch is $branch_remote"
+fi
+branch_local=$(git branch | grep \* | sed 's/^\* //')
+echo -e " local branch is \033[32m${branch_local}\033[m"
+
+git filter-repo $@ --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"lighthall@lsu.edu"]
     auth_list.append(b"jonlighthall@users.noreply.github.com")
@@ -11,3 +29,6 @@ git filter-repo $@ --partial --commit-callback '
         if commit.author_name != correct_name:
             commit.author_name = correct_name
 '
+echo "git remote add ${name_remote} ${url_remote}"
+echo "git fetch ${name_remote}"
+echo "git branch --set-upstream-to=${branch_tracking}"
