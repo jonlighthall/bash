a7b50971c2850c995203afdc3d256fc10fca1a56
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-04 14:59:27 -0500

    add pretty printing and branch parsing to git filters

--- git/filter-repo-author.sh
+++ git/filter-repo-author.sh
@@ -1,10 +1,25 @@
 set -e
+# print source name at start
+echo -n "source: $BASH_SOURCE"
+src_name=$(readlink -f $BASH_SOURCE)
+if [ "$BASH_SOURCE" = "$src_name" ]; then
+    echo
+else
+    echo " -> $src_name"
+fi
+
+# source formatting
+fpretty=${HOME}/utils/bash/.bashrc_pretty
+if [ -e $fpretty ]; then
+    source $fpretty
+fi
+
 # parse remote
 if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
     echo "no remote tracking branch"
 else
     branch_tracking=$(git branch -vv | grep \* | sed 's/^.*\[//;s/\(]\|:\).*$//')
-    echo -e "remote tracking branch is \033[34m${branch_tracking}\033[m"
+    echo -e "remote tracking branch is ${blue}${branch_tracking}${NORMAL}"
     name_remote=${branch_tracking%%/*}
     echo "remote is name $name_remote"
     url_remote=$(git remote -v | grep ${name_remote} |  awk '{print $2}' | sort -u)
@@ -14,7 +29,7 @@ else
     echo "remote branch is $branch_remote"
 fi
 branch_local=$(git branch | grep \* | sed 's/^\* //')
-echo -e " local branch is \033[32m${branch_local}\033[m"
+echo -e " local branch is ${green}${branch_local}${NORMAL}"
 
 branch_list=$(git branch -va | sed 's/^*/ /' |  awk '{print $1}' | sed 's|remotes/.*/||' | sort -u | sed '/HEAD/d')
 echo "list of branches: "
