5f786695afedcc5ad153c2175d66ac4aa14fca5b
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-10 16:16:59 -0500

    add tab, stash, rev list left/right check

--- git/filter-repo-author-loop.sh
+++ git/filter-repo-author-loop.sh
@@ -36,48 +36,75 @@ branch_list=$(git branch -va | sed 's/^*/ /' |  awk '{print $1}' | sed 's|remote
 echo "list of branches: "
 echo "${branch_list}" | sed 's/^/   /'
 
+if [ -z "$(git diff)" ]; then
+    echo "no differences to stash"
+    b_stash=false
+else
+    git stash
+    b_stash=true
+fi
+
 for branch in $branch_list
 do
     git checkout $branch
     git fetch ${name_remote} ${branch}
-    git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     git diff ${branch} ${name_remote}/${branch}
 
+    echo "remote:"
+    git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     N=$(git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort -u | wc -l)
     if [ $N -gt 1 ]; then
 	echo "${TAB}more than one author on remote branch ${name_remote}/${branch} (N=$N)"
-	echo "filtering repo and force pushing..."
+	echo "${TAB}filtering repo and force pushing..."
 	filter-repo-author.sh $@
-	echo "done filtering repo."
-	echo "force pushing rewrite..."
+	echo "${TAB}done filtering repo."
+	echo "${TAB}force pushing rewrite..."
 	git push -f ${name_remote} ${branch}
     else
 	echo "${TAB}only one author on remote branch ${name_remote}/${branch}!"
-	echo "no need to force push"
+	echo "${TAB}no need to force push"
+	echo "local:"
 	git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
 	M=$(git log --pretty=format:"%aN %aE" | sort -u | wc -l)
 	if [ $M -gt 1 ]; then
-	    echo "more than one author on local branch ${branch} (M=$M)"
-	    ehco "force pull..."
+	    echo "${TAB}more than one author on local branch ${branch} (M=$M)"
+	    ehco "${TAB}force pull..."
 	    force_pull
 	else
 	    echo "${TAB}only one author on local branch ${branch}!"
-	    echo "no need to force pull"
+	    echo "${TAB}no need to force pull"
 	fi
 
-	if [ N==1 ] && [ M==1 ] && [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
+	if [ N==1 ] && [ M==1 ]; then
 	    echo "only one author on local and remote"
-	    echo "no differences between local and remote" 
-	    echo "reseting HEAD to ${name_remote}/${branch}..."
-	    git reset ${name_remote}/${branch}
-	    git pull ${name_remote} ${branch}
-	    git push ${name_remote}
+
 	    if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
 		echo "no remote tracking branch"
 		git branch --set-upstream-to=${name_remote}/${branch} ${branch}
 	    fi
+
+	    if [ -z $(git rev-list --left-only ${name_remote}/${branch}...${branch}) ]; then
+		echo "no commits to pull"
+	    else
+		echo "pulling commits"
+		git pull ${name_remote} ${branch}
+	    fi
+
+	    if [ -z $(git rev-list --right-only ${name_remote}/${branch}...${branch}) ]; then
+		echo "no commits to push"
+	    else
+		echo "pushing commits"
+		git push ${name_remote}
+	    fi
+
+	    if [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
+		echo "no differences between local and remote"
+		echo "reseting HEAD to ${name_remote}/${branch}..."
+		git reset ${name_remote}/${branch}
+	    else
+		echo "unsafe to reset"
+	    fi
 	else
-	    echo "unsafe to reset"
 	    echo "remote authors N=$N"
 	    echo " local authors M=$M"
 	fi
@@ -86,3 +113,6 @@ done
 echo "done"
 echo "switching back to ${branch_local}..."
 git checkout ${branch_local}
+if $b_stash; then
+    git stash pop
+fi
