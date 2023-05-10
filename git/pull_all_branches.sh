870ab914dc250019594405d26339521e3fe7ec76
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-10 16:36:38 -0500

    add comments and hash check; merge changes into pull all

--- git/pull_all_branches.sh
+++ git/pull_all_branches.sh
@@ -17,6 +17,7 @@ fi
 # parse remote
 if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
     echo "no remote tracking branch"
+    exit 1
 else
     branch_tracking=$(git branch -vv | grep \* | sed 's/^.*\[//;s/\(]\|:\).*$//')
     echo -e "remote tracking branch is ${blue}${branch_tracking}${NORMAL}"
@@ -35,11 +36,18 @@ branch_list=$(git branch -va | sed 's/^*/ /' |  awk '{print $1}' | sed 's|remote
 echo "list of branches: "
 echo "${branch_list}" | sed 's/^/   /'
 
+if [ -z "$(git diff)" ]; then
+    echo "no differences to stash"
+    b_stash=false
+else
+    git stash
+    b_stash=true
+fi
+
 for branch in $branch_list
 do
     git checkout $branch
     git fetch ${name_remote} ${branch}
-    git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     git diff ${branch} ${name_remote}/${branch}
 
 # determine latest common local commit, based on commit message
@@ -129,40 +137,90 @@ else
     N_remote=0
 fi
 
-
-
-
-
-
-    
-
+# dummy loop to match formatting
+for i in 1;
+do
+    # determine number of authors on remote branch
+    echo "remote:"
+    git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
     N=$(git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort -u | wc -l)
     if [ $N -gt 1 ]; then
-	echo "${TAB}more than one author on remote branch ${name_remote}/${branch}"
+	echo "${TAB}more than one author on remote branch ${name_remote}/${branch} (N=$N)"
+	echo "${TAB}filtering repo..."
 	filter-repo-author.sh $@
+	echo "${TAB}done filtering repo"
+	echo "${TAB}force pushing rewrite..."
 	git push -f ${name_remote} ${branch}
     else
 	echo "${TAB}only one author on remote branch ${name_remote}/${branch}!"
+	echo "${TAB}no need to filter or (force) push"
+	# determine number of authors on local branch
+	echo "local:"
 	git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
 	M=$(git log --pretty=format:"%aN %aE" | sort -u | wc -l)
 	if [ $M -gt 1 ]; then
-	    echo "more than one author on local branch ${branch}"
-	    ehco "force pull..."
+	    echo "${TAB}more than one author on local branch ${branch} (M=$M)"
+	    ehco "${TAB}force pull..."
 	    force_pull
 	else
 	    echo "${TAB}only one author on local branch ${branch}!"
+	    echo "${TAB}no need to force pull"
 	fi
 
-	if [ N==1 ] && [ M==1 ] && [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
-	    echo "reseting HEAD to ${name_remote}/${branch}..."
-	    git reset ${name_remote}/${branch}
-	    git pull
-	    git push
+	if [ N==1 ] && [ M==1 ]; then
+	    echo "only one author on local and remote"
+
+	    # determine remote tracking branch
+	    if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
+		echo "no remote tracking branch"
+		git branch --set-upstream-to=${name_remote}/${branch} ${branch}
+	    fi
+
+	    # determine number commits local branch is behind remote
+	    if [ -z $(git rev-list --left-only ${name_remote}/${branch}...${branch}) ]; then
+		echo "no commits to pull"
+	    else
+		echo "pulling commits"
+		git pull ${name_remote} ${branch}
+	    fi
+
+	    # determine number commits local branch is ahead of remote
+	    if [ -z $(git rev-list --right-only ${name_remote}/${branch}...${branch}) ]; then
+		echo "no commits to push"
+	    else
+		echo "pushing commits"
+		git push ${name_remote}
+	    fi
+
+	    # determine difference between local and remote
+	    if [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
+		echo "no differences between local and remote"
+
+		hash_remote=$(git rev-parse ${name_remote}/${branch})
+		hash_local= $(git rev-parse HEAD)
+		echo -n "${TAB}local and remote hashes..."
+		if [[ "$hash_remote" == "$hash_local" ]]; then
+		    echo "${GOOD}match${NORMAL}"
+		else
+		    echo "${BAD}no not match${NORMAL}"
+		    echo $hash_local
+		    echo $hash_remote
+		    
+		    echo "reseting HEAD to ${name_remote}/${branch}..."
+		    git reset ${name_remote}/${branch}
+		fi
+	    else
+		echo "unsafe to reset"
+	    fi
 	else
-	    echo "unsafe to reset"
+	    echo "remote authors N=$N"
+	    echo " local authors M=$M"
 	fi
     fi
 done
 echo "done"
 echo "switching back to ${branch_local}..."
 git checkout ${branch_local}
+if $b_stash; then
+    git stash pop
+fi
