2613ddd37d56b6c9c2029d190bba15ec3b50c2ef
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-11 15:36:53 -0500

    add display to whatsup

--- whatsup.sh
+++ whatsup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "   host:" $HOSTNAME
+echo "display: $DISPLAY"
 echo -n "     IP: ";hostname -I
 echo -n "     OS: "
 lsb_release -a 2>&1 | \grep "Description:" | sed -e 's/^Description:[\t]//'
