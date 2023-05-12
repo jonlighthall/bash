1cd7d6d902b251ba59480a17450b8892532ec2c0
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-11 20:29:34 -0500

    working for loop with cursor movement!

--- for_loop.sh
+++ for_loop.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-N=5
+N=5000
 echo "start"
-echo -e "counting to $N... \x1B[s"
+echo -e "counting to $N... \x1B[s+++"
 for ((i=1;i<=$N;i++))
 do
-    echo -en "$i: x"
-#    sleep 1
+    echo -en "   $i: x"
     echo -en "y"
- #   sleep 1
     echo -en "z"
-    #    echo -e "xy\x1B[Kz"
     if [ "$i" -lt "$N" ];then
 	echo -en " $i < $N"
-#	sleep 1
-#	echo -en "\x1B[K"
-	echo
+	echo -en "\x1B[K"
     else
 	echo " $i >= $N"
 	echo "done"
-	#echo -e "\x1B[3F\x1B[1D"
 	echo -en "\x1B[u"
+	sleep 1
 	echo "here"
-	echo -en "\x1B[$((N+1))B"
+	echo -e "\x1B[E"
     fi
 done
 echo "one more line"
