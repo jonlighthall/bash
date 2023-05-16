2566475988ae92286e72d87dce67a3487cf6fa41
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-16 10:16:33 -0500

    remove move parts (new file) and automatically determine outputfile

--- find_matching.sh
+++ find_matching.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 #
 # find_matching.sh - Reads an input list of files name patterns. Any files matching the
-# individual patterns are saved to a new list of file names. handling is included to backup any
+# individual patterns are saved to a new list of file names. Handling is included to backup any
 # duplicated input/output file names.
 #
 # Adapted from find_missing_and_empty.sh
 #
 # First argument is the list file of patterns
 #
-# Second argument is the name of the output file
+# Second argument is the search directory or file
 #
-# Third argument specifies the named sub-directory to move the matching files
 #
 # Use example:
 # find_matching list_of_patterns.txt output.txt
-# find_matching output.txt dummy subdir
 #
-# the first command will locate the files matching the pattern and write the matches to file. the
-# second command will take the located files specified by the list and move them to the
-# sub-directory subdir. For example below, the search pattern (first line) matches the file name
-# (second line) found by running the first command. The file is then moved to the new
-# sub-directory with the second command.
+# the command will locate the files matching the pattern and write the matches to file. For
+# example below, the search pattern (first line) matches the file name (second line) found by
+# running the command.
 #
 # file_name_???
 # dir/new_file_name123.bin
-# dir/subdir/new_file_name123.bin
 #
 #JCL Apr 2023
 
 TAB="   "
-# set file names
-file_in=$1
 
 # initialize counters
 k=0 # files in list
@@ -40,83 +33,98 @@ if [ $# -eq 0 ]; then
     echo "Please provide an input file"
     exit 1
 else
-    # second argument (output) becomes dummy variable if third argument is specified
-    if [ $# -lt 3 ]; then
-	if [ $# -ge 2 ]; then
-	    file_spec=$2
-	else
-	    # set default output file name to match input
-	    base1="${1%.*}"
-	    if [[ "$1" == *"."* ]]; then
-		ext1="${1##*.}"
-	    else
-		ext1="txt"
-	    fi
-	    file_spec="${base1}_found.${ext1}"
-	fi
-	base="${file_spec%.*}"
-	if [[ "{file_spec}" == *"."* ]]; then
-	    ext="${file_spec##*.}"
-	else
-	    ext="txt"
-	fi
+    echo "number of arguments = $#"
 
-	file_out=${base}.${ext}
+    # set file names
+    file_in=$(readlink -f $1)
+    echo "$1"
+    echo "${file_in}"
 
-	# check if input and output are the same file
-	echo -n " input file ${file_in} is... "
-	while [ ${file_in} -ef ${file_out} ]; do
-            echo "the same file as ${file_out}"
-            echo -n "${TAB}renaming output... "
-	    file_out=${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
-	    echo ${file_out}
-	done
-	echo "OK"
 
-	# check if output exists
-	echo -n "output file ${file_out}... "
-	while [ -f ${file_out} ]; do
-            echo "exists"
-            echo -n "${TAB}renaming output... "
-	    file_out=${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
-	    echo ${file_out}
-	done
-	echo "OK"
+    # set default output file name to match input
+    dir1=$(dirname $file_in)
+    echo "dir = $dir1"
+    fname1=$(basename $file_in)
+    echo "input file = $fname1"
+    base="${fname1%.*}"
+    echo "base name = $base"
+    if [[ $fname1 == *"."* ]]; then
+	echo "fname contains dots"
+	ext1="${fname1##*.}"
+    else
+	echo "fname contains not dots, using default"
+	ext1="txt"
     fi
+    file_spec="${dir1}/${base}_found.${ext1}"
+
+    echo "file specification = $file_spec"
+
+
+    file_out=${file_spec}
+    echo $file_out
+
+    # check if input and output are the same file
+    echo -n " input file ${file_in} is... "
+    while [ ${file_in} -ef ${file_out} ]; do
+        echo "the same file as ${file_out}"
+        echo -n "${TAB}renaming output... "
+	file_out=${dir1}/${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
+	echo ${file_out}
+    done
+    echo "OK"
+
+    # check if output exists
+    echo -n "output file ${file_out}... "
+    while [ -f ${file_out} ]; do
+        echo "exists"
+        echo -n "${TAB}renaming output... "
+	file_out=${dir1}/${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
+	echo ${file_out}
+    done
+    echo "OK"
 
     # read input file
+    j=$(cat ${file_in} | wc -l)
+    echo " input file ${file_in} has $j entries"
+
+    if [ $# -ge 2 ]; then
+	search_dir=$(readlink -f $2)
+    else
+	search_dir=$(readlink -f $PWD)
+    fi
+    echo -n "search directory ${search_dir}... "
+    if [ -d $search_dir ];then
+	echo "OK"
+    else
+	echo "not found"
+	exit 1
+    fi
+#    exit 0
+    nprint=$((j/10))
+    echo "printing every $nprint lines"
     while read line; do
         fname=$line
         ((k++))
-	echo -n "$k looking for ${fname}... "
-	# if third argument present, mv file
-	if [ $# -ge 3 ]; then
-	    dir_par=${fname%/*}
-	    dir_mv=${dir_par}/$3
-	    if ! [ -d ${dir_mv} ]; then
-		echo "${dir_mv} not found"
-		mkdir -pv ${dir_mv}
-	    fi
-	    if [ -f ${fname} ]; then
-		echo
-#		echo -n ${TAB}
-		mv -v ${fname} ${dir_mv} | sed "s/^/${TAB}/"
-	    else
-		echo "not found"
-	    fi
-
-	    # otherwise write match to file
+	if [ $(( k % $nprint)) -eq 0 ]; then
+	    echo -ne "\n$k/$j looking for ${fname}... "
 	else
-	    find ./ -type f -name *${fname}* >> ${file_out}
-	    echo "done"
+	    echo -n ","
 	fi
-
+	find $search_dir -type f -name *${fname}* >> ${file_out}
+#	echo "$?"
+	if [ $(( k % $nprint)) -eq 0 ]; then
+	    echo "done"
+	else
+	    echo -n "."
+	fi
     done < $file_in
     echo
     echo $k "file names checked"
-    if [ $# -lt 3 ]; then
-	echo $(cat ${file_out} | wc -l) "files found"
-    fi
+    echo "$((j-k)) files not searched for"
+    l=$(cat ${file_out} | wc -l)
+    echo "$l files found"
+    echo "$((j-l)) files not found"
+
 fi
 # print time at exit
 echo -e "\n$(date +"%R") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
