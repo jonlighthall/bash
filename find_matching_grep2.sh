4b210c271c993c3cb94d9ddcd0d5bf0c5d325ba0
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-16 10:17:51 -0500

    add automatic output file name, including directory, add count files

--- find_matching_grep2.sh
+++ find_matching_grep2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # find_matching.sh - Reads an input list of files name patterns. Any files matching the
-# individual patterns are saved to a new list of file names. handling is included to backup any
+# individual patterns are saved to a new list of file names. Handling is included to backup any
 # duplicated input/output file names.
 #
 # Adapted from find_missing_and_empty.sh
@@ -13,7 +13,6 @@
 #
 # Use example:
 # find_matching list_of_patterns.txt output.txt
-
 #
 # the command will locate the files matching the pattern and write the matches to file. For
 # example below, the search pattern (first line) matches the file name (second line) found by
@@ -25,8 +24,6 @@
 #JCL Apr 2023
 
 TAB="   "
-# set file names
-file_in=$1
 
 # initialize counters
 k=0 # files in list
@@ -36,23 +33,42 @@ if [ $# -eq 0 ]; then
     echo "Please provide an input file"
     exit 1
 else
+    echo "number of arguments = $#"
 
-    file_spec=found.txt
-    base="${file_spec%.*}"
-    if [[ "{file_spec}" == *"."* ]]; then
-	ext="${file_spec##*.}"
+    # set file names
+    file_in=$(readlink -f $1)
+    echo "$1"
+    echo "${file_in}"
+
+
+    # set default output file name to match input
+    dir1=$(dirname $file_in)
+    echo "dir = $dir1"
+    fname1=$(basename $file_in)
+    echo "input file = $fname1"
+    base="${fname1%.*}"
+    echo "base name = $base"
+    if [[ $fname1 == *"."* ]]; then
+	echo "fname contains dots"
+	ext1="${fname1##*.}"
     else
-	ext="txt"
+	echo "fname contains not dots, using default"
+	ext1="txt"
     fi
+    file_spec="${dir1}/${base}_found.${ext1}"
 
-    file_out=${base}.${ext}
+    echo "file specification = $file_spec"
+
+
+    file_out=${file_spec}
+    echo $file_out
 
     # check if input and output are the same file
     echo -n " input file ${file_in} is... "
     while [ ${file_in} -ef ${file_out} ]; do
         echo "the same file as ${file_out}"
         echo -n "${TAB}renaming output... "
-	file_out=${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
+	file_out=${dir1}/${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
 	echo ${file_out}
     done
     echo "OK"
@@ -62,12 +78,15 @@ else
     while [ -f ${file_out} ]; do
         echo "exists"
         echo -n "${TAB}renaming output... "
-	file_out=${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
+	file_out=${dir1}/${base}_$(date +'%Y-%m-%d-t%H%M%S').${ext}
 	echo ${file_out}
     done
     echo "OK"
 
     # read input file
+    j=$(cat ${file_in} | wc -l)
+    echo " input file ${file_in} has $j entries"
+
     while read line; do
         fname=$line
         ((k++))
@@ -88,11 +107,14 @@ else
 		exit 1
 	    fi
 	fi
-
     done < $file_in
     echo
     echo $k "file names checked"
-    echo $(cat ${file_out} | wc -l) "files found"
+    echo "$((j-k)) files not searched for"
+    l=$(cat ${file_out} | wc -l)
+    echo "$l files found"
+    echo "$((j-l)) files not found"
+
 fi
 # print time at exit
 echo -e "\n$(date +"%R") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
