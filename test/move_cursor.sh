a1457ec5b80d74de74df310f503036cca87a4164
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-17 12:02:57 -0500

    add hash bang and indent

--- test/move_cursor.sh
+++ test/move_cursor.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 echo -en "\x1B[1;34mhere\x1B[0m"
- echo -en "\E[6n"
+echo -en "\E[6n"
 read -sdR CURPOS
- CURPOS=${CURPOS#*[}
+CURPOS=${CURPOS#*[}
 echo "${CURPOS}"
 cx=${CURPOS#*;}
 cy=${CURPOS%;*}
@@ -9,7 +10,7 @@ echo "position is x=$cx y=$cy"
 ls -la
 echo -en "\E[6n"
 read -sdR CURPOS
- CURPOS=${CURPOS#*[}
+CURPOS=${CURPOS#*[}
 cx1=${CURPOS#*;}
 cy1=${CURPOS%;*}
 echo -e "\x1B[${cy};${cx}H \033[1;31mthere $cx $cy\033[0m"
