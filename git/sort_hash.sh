bfc5d77594a9258e44e73ccf24d7eed03048d426
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-21 16:52:49 -0500

    add sort and apply hashes, May 18 17;05

---
+++ git/sort_hash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while read -r line
+do
+    echo "$line"
+    ts1=$(echo $line | awk '{print $1}')
+    echo "timestamp = $ts1"
+    if [ $ts1 = $ts2 ]; then
+	echo "match"
+    else
+	echo "new"
+	ts2=${ts1}
+	echo $line | awk '{print $2}' >> good_hashes.txt
+    fi
+done < hash_list2.txt
