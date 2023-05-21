567fed70388cd9a6126ee16ea2087dc402a2c335
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-21 17:22:23 -0500

    mostly working: replace all resets

--- test/get_wttr.sh
+++ test/get_wttr.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl wttr.in 2>&1 | sed "s/^/\x1b[40m/" 
+curl wttr.in 2>/dev/null | sed "s/^/\x1B[40;37m/" | sed 's/\x1B\[0m/\x1B[40;37m/g'
+#curl wttr.in 2>/dev/null | sed 's/\x1B\[0m/\x1B[40m/g'
+echo -e "\x1b[0m"
