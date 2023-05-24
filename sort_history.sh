8839ef5cf55d3e6b0993d9d5d017f73a1362895f
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-24 11:58:44 -0500

    add hist save format

--- sort_history.sh
+++ sort_history.sh
@@ -195,7 +195,7 @@ sed -i "s/${TS_MARKER}/\n/;s/${OR_MARKER}/\n/g" ${hist_out}
 echo "done"
 
 # save markers
-echo "#$(date +'%s') SORT $(date +'%a %b %d %Y %R:%S %Z') using markers ${TS_MARKER} ${OR_MARKER} (LC_COLLATE = ${LCcol})" >> ${hist_out}
+echo "#$(date +'%s') SORT   $(date +'%a %b %d %Y %R:%S %Z') using markers ${TS_MARKER} ${OR_MARKER} (LC_COLLATE = ${LCcol})" >> ${hist_out}
 
 cp -Lpv ${hist_out} ${hist_ref}
 
