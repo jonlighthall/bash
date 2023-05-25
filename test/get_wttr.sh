a62b34824d7b5af6f21c40a751ca14ecff24ec52
Jon Lighthall <jon.lighthall@gmail.com>
2023-05-25 17:38:22 -0500

    fix get wether email print

--- test/get_wttr.sh
+++ test/get_wttr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl wttr.in 2>/dev/null | sed 's/^/\x1B[40;37m/;s/\x1B\[0m/\x1B[40;37m/g'
+curl wttr.in 2>/dev/null | sed 's/^/\x1B[40;37m/;s/\x1B\[0m/\x1B[40;37m/g;s/46/0;46/'
 echo -e "\x1b[0m"
