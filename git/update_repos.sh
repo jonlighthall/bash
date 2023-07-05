3254f388d4ffe198515eeee8aadf110191a97457
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-05 12:58:48 -0500

    unify date

--- git/update_repos.sh
+++ git/update_repos.sh
@@ -212,4 +212,4 @@ else
 fi
 
 # print time at exit
-echo -e "\n$(date +"%a %b %d %I:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
+echo -e "\n$(date +"%a %b %-d %I:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
