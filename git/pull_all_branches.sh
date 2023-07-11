9aff8f0cabd57cc2bad05be799d621a6a9df0adb
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-11 15:09:08 -0500

    indent to min diff

--- git/pull_all_branches.sh
+++ git/pull_all_branches.sh
@@ -86,193 +86,192 @@ do
 done
 exit
 
-continue
 
 # determine latest common local commit, based on commit message
-    tracking==${name_remote}/${branch_remote}
-    while [ -z ${hash_local} ]; do
-	echo "pulling from ${tracking}"
-	subj_remote=$(git log ${tracking} --format=%s -n 1)
-	time_remote=$(git log ${tracking} --format=%at -n 1)
-	echo "${TAB}remote commit subject: $subj_remote"
+tracking==${name_remote}/${branch_remote}
+while [ -z ${hash_local} ]; do
+    echo "pulling from ${tracking}"
+    subj_remote=$(git log ${tracking} --format=%s -n 1)
+    time_remote=$(git log ${tracking} --format=%at -n 1)
+    echo "${TAB}remote commit subject: $subj_remote"
 
-	hash_local=$(git log | grep -B4 "$subj_remote" | head -n 1 | awk '{print $2}')
+    hash_local=$(git log | grep -B4 "$subj_remote" | head -n 1 | awk '{print $2}')
 
-	hash_local_t=$(git log --format="%at %H " | grep "$time_remote" | awk '{print $2}')
+    hash_local_t=$(git log --format="%at %H " | grep "$time_remote" | awk '{print $2}')
 
-	echo "subject and time hashes..."
-	if [ "$hash_local" == "$hash_local_t" ]; then
-	    echo "match"
-	else
-	    echo "do not match"
-	    echo $hash_local
-	    echo $hash_local_t
-	    exit 1
-	fi
-	echo -n "${TAB}corresponding local commit hash: "
-	if [ ! -z ${hash_local} ]; then
-	    echo "$hash_local"
-	    echo -n "${TAB}trailing local commits: "
-	    hash_start=$(git rev-list $hash_local..HEAD | tail -n 1)
-	    if [ ! -z ${hash_start} ]; then
-		echo
-		git rev-list $hash_local..HEAD | sed "s/^/${TAB}/"
-		N_local=$(git rev-list $hash_local..HEAD | wc -l)
-		if [ $N_local -gt 1 ]; then
-		    echo -n "or ${hash_start}^.."
-		    hash_end=$(git rev-list $hash_local..HEAD | head -n 1)
-		    echo ${hash_end}
-		else
-		    hash_end=$hash_start
-		fi
-		echo "${TAB}local branch is $N_local commits ahead of remote"
+    echo "subject and time hashes..."
+    if [ "$hash_local" == "$hash_local_t" ]; then
+	echo "match"
+    else
+	echo "do not match"
+	echo $hash_local
+	echo $hash_local_t
+	exit 1
+    fi
+    echo -n "${TAB}corresponding local commit hash: "
+    if [ ! -z ${hash_local} ]; then
+	echo "$hash_local"
+	echo -n "${TAB}trailing local commits: "
+	hash_start=$(git rev-list $hash_local..HEAD | tail -n 1)
+	if [ ! -z ${hash_start} ]; then
+	    echo
+	    git rev-list $hash_local..HEAD | sed "s/^/${TAB}/"
+	    N_local=$(git rev-list $hash_local..HEAD | wc -l)
+	    if [ $N_local -gt 1 ]; then
+		echo -n "or ${hash_start}^.."
+		hash_end=$(git rev-list $hash_local..HEAD | head -n 1)
+		echo ${hash_end}
 	    else
-		echo "none"
-		N_local=0
+		hash_end=$hash_start
 	    fi
+	    echo "${TAB}local branch is $N_local commits ahead of remote"
 	else
-	    echo "not found"
+	    echo "none"
+	    N_local=0
 	fi
-	tracking="${tracking}~"
-    done
+    else
+	echo "not found"
+    fi
+    tracking="${tracking}~"
+done
 
 # determine local commits not found on remote
-    hash_remote=$(git log ${branch_pull} | grep -B4 "${subj_remote}" | head -n 1 | awk '{print $2}')
-    echo -n "${TAB}corresponding remote commit hash: "
-    echo $hash_remote
-    echo -n "common commit has... "
-    if [ $hash_local == $hash_remote ]; then
-	echo "the same hash"
-	git merge-base ${branch_local} ${branch_pull}
-	hash_merge=$(git merge-base ${branch_local} ${branch_pull})
-	echo -n "common hash is... "
-	if [ $hash_local == $hash_merge ]; then
-	    echo "the same as merge base"
-	else
-	    echo "not the same as merge base"
-	fi
+hash_remote=$(git log ${branch_pull} | grep -B4 "${subj_remote}" | head -n 1 | awk '{print $2}')
+echo -n "${TAB}corresponding remote commit hash: "
+echo $hash_remote
+echo -n "common commit has... "
+if [ $hash_local == $hash_remote ]; then
+    echo "the same hash"
+    git merge-base ${branch_local} ${branch_pull}
+    hash_merge=$(git merge-base ${branch_local} ${branch_pull})
+    echo -n "common hash is... "
+    if [ $hash_local == $hash_merge ]; then
+	echo "the same as merge base"
     else
-	echo "a different hash (diverged)"
+	echo "not the same as merge base"
     fi
+else
+    echo "a different hash (diverged)"
+fi
 
 # determine remote commits not found locally
-    echo -n "${TAB}leading remote commits: "
-    hash_start_remote=$(git rev-list $hash_remote..${branch_pull} | tail -n 1)
-    if [ ! -z ${hash_start_remote} ]; then
-	echo
-	git rev-list $hash_remote..${branch_pull} | sed "s/^/${TAB}/"
-	N_remote=$(git rev-list $hash_remote..${branch_pull} | wc -l)
-	if [ $N_remote -gt 1 ]; then
-	    echo -n "or ${hash_start_remote}^.."
-	    hash_end_remote=$(git rev-list $hash_remote..${branch_pull} | head -n 1)
-	    echo ${hash_end_remote}
-	else
-	    hash_end_remote=$hash_start_remote
-	fi
-	echo "${TAB}remote branch is $N_remote commits ahead of local"
+echo -n "${TAB}leading remote commits: "
+hash_start_remote=$(git rev-list $hash_remote..${branch_pull} | tail -n 1)
+if [ ! -z ${hash_start_remote} ]; then
+    echo
+    git rev-list $hash_remote..${branch_pull} | sed "s/^/${TAB}/"
+    N_remote=$(git rev-list $hash_remote..${branch_pull} | wc -l)
+    if [ $N_remote -gt 1 ]; then
+	echo -n "or ${hash_start_remote}^.."
+	hash_end_remote=$(git rev-list $hash_remote..${branch_pull} | head -n 1)
+	echo ${hash_end_remote}
     else
-	echo "none"
-	N_remote=0
+	hash_end_remote=$hash_start_remote
     fi
+    echo "${TAB}remote branch is $N_remote commits ahead of local"
+else
+    echo "none"
+    N_remote=0
+fi
 
 # dummy loop to match formatting
-    for i in 1;
-    do
+for i in 1;
+do
     # determine number of authors on remote branch
-	echo "remote:"
-	git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
-	N=$(git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort -u | wc -l)
-	if [ $N -gt 1 ]; then
-	    echo "${TAB}${GRH}more than one author on remote branch ${name_remote}/${branch} (N=$N)${NORMAL}"
-	    echo "${TAB}filtering repo..."
-	    ${HOME}/utils/bash/git/filter-repo-author.sh $@
-	    echo "${TAB}done filtering repo"
-	    echo "${TAB}force pushing rewrite..."
-	    git push -f ${name_remote} ${branch}
-	else
-	    echo "${TAB}only one author on remote branch ${name_remote}/${branch}!"
-	    echo "${TAB}no need to filter or (force) push"
+    echo "remote:"
+    git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+    N=$(git log ${name_remote}/${branch} --pretty=format:"%aN %aE" | sort -u | wc -l)
+    if [ $N -gt 1 ]; then
+	echo "${TAB}${GRH}more than one author on remote branch ${name_remote}/${branch} (N=$N)${NORMAL}"
+	echo "${TAB}filtering repo..."
+	${HOME}/utils/bash/git/filter-repo-author.sh $@
+	echo "${TAB}done filtering repo"
+	echo "${TAB}force pushing rewrite..."
+	git push -f ${name_remote} ${branch}
+    else
+	echo "${TAB}only one author on remote branch ${name_remote}/${branch}!"
+	echo "${TAB}no need to filter or (force) push"
 	# determine number of authors on local branch
-	    echo "local:"
-	    git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
-	    M=$(git log --pretty=format:"%aN %aE" | sort -u | wc -l)
-	    if [ $M -gt 1 ]; then
-		echo "${TAB}more than one author on local branch ${branch} (M=$M)"
-		ehco "${TAB}force pull..."
-		force_pull
-	    else
-		echo "${TAB}only one author on local branch ${branch}!"
-		echo "${TAB}no need to force pull"
-	    fi
+	echo "local:"
+	git log --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+	M=$(git log --pretty=format:"%aN %aE" | sort -u | wc -l)
+	if [ $M -gt 1 ]; then
+	    echo "${TAB}more than one author on local branch ${branch} (M=$M)"
+	    ehco "${TAB}force pull..."
+	    force_pull
+	else
+	    echo "${TAB}only one author on local branch ${branch}!"
+	    echo "${TAB}no need to force pull"
+	fi
 
-	    if [ N==1 ] && [ M==1 ]; then
-		echo "only one author on local and remote"
+	if [ N==1 ] && [ M==1 ]; then
+	    echo "only one author on local and remote"
 
 	    # determine remote tracking branch
-		if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
-		    echo "no remote tracking branch"
-		    git branch --set-upstream-to=${name_remote}/${branch} ${branch}
-		fi
+	    if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
+		echo "no remote tracking branch"
+		git branch --set-upstream-to=${name_remote}/${branch} ${branch}
+	    fi
 
-		ver=$(git --version | awk '{print $3}')
-		ver_maj=$(echo $ver | awk -F. '{print $1}')
-		ver_min=$(echo $ver | awk -F. '{print $2}')
-		ver_pat=$(echo $ver | awk -F. '{print $3}')
+	    ver=$(git --version | awk '{print $3}')
+	    ver_maj=$(echo $ver | awk -F. '{print $1}')
+	    ver_min=$(echo $ver | awk -F. '{print $2}')
+	    ver_pat=$(echo $ver | awk -F. '{print $3}')
 
 	    # determine number commits local branch is behind remote
-		if [ $ver_maj -lt 2 ]; then
+	    if [ $ver_maj -lt 2 ]; then
+		echo "pulling commits"
+		git pull ${name_remote} ${branch}
+	    else
+		if [ -z $(git rev-list --left-only ${name_remote}/${branch}...${branch}) ]; then
+		    echo "no commits to pull"
+		else
 		    echo "pulling commits"
 		    git pull ${name_remote} ${branch}
-		else
-		    if [ -z $(git rev-list --left-only ${name_remote}/${branch}...${branch}) ]; then
-			echo "no commits to pull"
-		    else
-			echo "pulling commits"
-			git pull ${name_remote} ${branch}
-		    fi
 		fi
+	    fi
 
 	    # determine number commits local branch is ahead of remote
-		if [ $ver_maj -lt 2 ]; then
+	    if [ $ver_maj -lt 2 ]; then
+		echo "pushing commits"
+		git push ${name_remote}
+		if [ -z $(git rev-list --right-only ${name_remote}/${branch}...${branch}) ]; then
+		    echo "no commits to push"
+		else
 		    echo "pushing commits"
 		    git push ${name_remote}
-		    if [ -z $(git rev-list --right-only ${name_remote}/${branch}...${branch}) ]; then
-			echo "no commits to push"
-		    else
-			echo "pushing commits"
-			git push ${name_remote}
-		    fi
 		fi
+	    fi
 
 	    # determine difference between local and remote
-		if [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
-		    echo "no differences between local and remote"
+	    if [ -z "$(git diff ${branch} ${name_remote}/${branch})" ]; then
+		echo "no differences between local and remote"
 
-		    hash_remote=$(git rev-parse ${name_remote}/${branch})
-		    hash_local=$(git rev-parse HEAD)
-		    echo -n "${TAB}local and remote hashes..."
-		    if [[ "$hash_remote" == "$hash_local" ]]; then
-			echo -e "${GOOD}match${NORMAL}"
-		    else
-			echo -e "${BAD}no not match${NORMAL}"
-			echo $hash_local
-			echo $hash_remote
-
-			echo "reseting HEAD to ${name_remote}/${branch}..."
-			git reset ${name_remote}/${branch}
-		    fi
+		hash_remote=$(git rev-parse ${name_remote}/${branch})
+		hash_local=$(git rev-parse HEAD)
+		echo -n "${TAB}local and remote hashes..."
+		if [[ "$hash_remote" == "$hash_local" ]]; then
+		    echo -e "${GOOD}match${NORMAL}"
 		else
-		    echo "unsafe to reset"
+		    echo -e "${BAD}no not match${NORMAL}"
+		    echo $hash_local
+		    echo $hash_remote
+
+		    echo "reseting HEAD to ${name_remote}/${branch}..."
+		    git reset ${name_remote}/${branch}
 		fi
 	    else
-		echo "remote authors N=$N"
-		echo " local authors M=$M"
+		echo "unsafe to reset"
 	    fi
+	else
+	    echo "remote authors N=$N"
+	    echo " local authors M=$M"
 	fi
-    done
-    echo "done"
-    echo "switching back to ${branch_local}..."
-    bar 56 "$(git checkout ${branch_local} 2>&1)"
-    if $b_stash; then
-	git stash pop
     fi
+done
+echo "done"
+echo "switching back to ${branch_local}..."
+bar 56 "$(git checkout ${branch_local} 2>&1)"
+if $b_stash; then
+    git stash pop
+fi
