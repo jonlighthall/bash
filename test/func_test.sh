03eac70a2efe8cb1d9fa537620ce96323a56ef2a
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-28 16:47:25 -0500

    add function test

---
+++ test/func_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+hello() {
+    echo "world"
+}
+
+for func in hello bar hline potato
+do
+    echo "testing if $func is a function"
+    if [ "$(type -t $func)" != function ]; then
+	echo "$func not a function"
+	eval "$func() {
+	    :
+	}"
+    else
+	echo "$func is a function:"
+	$func
+    fi
+
+done
