d5ae3087063ba2ddd630c71c3f2ca779bde1539c
Jon Lighthall <jon.lighthall@gmail.com>
2023-07-31 14:38:30 -0500

    remove subshells for sec2elap

--- test/test_sort.sh
+++ test/test_sort.sh
@@ -162,7 +162,7 @@ done
 # print time at exit
 echo -en "$(date +"%a %b %-d %I:%M %p %Z") ${BASH_SOURCE##*/} "
 if command -v sec2elap &>/dev/null; then
-    echo "$(sec2elap $SECONDS)"
+    sec2elap ${SECONDS}
 else
     echo "elapsed time is ${SECONDS} sec"
 fi
