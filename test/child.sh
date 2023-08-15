88bf14f10ca5c43cb36b453d29e4207196aad820
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-14 21:08:18 -0500

    update prints on parent/child

--- test/child.sh
+++ test/child.sh
@@ -1,72 +1,93 @@
 #!/bin/bash
-set -e # exit on non-zero status
 start_time=$(date +%s%N)
 # print source name at start
+echo
 echo "source----------------------"
 if (return 0 2>/dev/null); then
     RUN_TYPE="sourcing"
 else
-    RUN_TYPE="executing"
+    RUN_TYPE="executing (not sourced)"
+    set -e # exit on non-zero status
 fi
-echo -e "${TAB}${RUN_TYPE} ${PSDIR}$BASH_SOURCE${NORMAL}..."
+
+echo -e "\$BASH_SOURCE = $BASH_SOURCE"
 src_name=$(readlink -f $BASH_SOURCE)
 if [ ! "$BASH_SOURCE" = "$src_name" ]; then
-    echo -e "${TAB}${VALID}link${NORMAL} -> $src_name"
+    echo -e "        ${VALID}link${NORMAL} = $src_name"
 fi
+echo -e "          \$0 = ${0}"
 
-
-echo -e "${TAB}${RUN_TYPE} ${PSDIR}$BASH_SOURCE${NORMAL}..."
-echo "  running ${0}"
-echo "running ${0##*/}"
-
-
-
+echo "    \$0 running ${0##*/}"
+echo
 echo "directory--------------------"
-echo "in ${0%/*}"
-
+echo -e "     \$ directory ${0%/*}"
 source_dir=$(dirname $BASH_SOURCE)
 echo "source directory ${source_dir}"
 link_dir=$(dirname "${src_name}")
 echo "  link directory ${link_dir}"
 real_dir=$(dirname "$(realpath $BASH_SOURCE)"	 )
 echo "  real directory ${real_dir}"
-
 start_dir=$PWD
 echo "             PWD ${start_dir}"
-
 echo "         logical" $(\pwd -L)
 echo "        physical" $(\pwd -P)
 
+echo
 echo "parent----------------------"
-
+echo "compare shell/script to source..."
 if [ "${0}" = "$BASH_SOURCE"  ];then
-    echo "   0 same as bash source"
+    echo -e "   \$0 same as \$BASH_SOURCE"
     echo "   This script is called by another process."
 else
-    echo "   0 NOT same as bash source"
+    echo -e "   \$0 NOT same as \$BASH_SOURCE"
     echo "   This script is not called by another process."
 fi
 
-echo "       PID = $$"
-echo "parent PID = $PPID"
+echo "compare PID..."
+echo "          PID = $$"
+echo "   parent PID = $PPID"
+if [ "$$" = "$PPID"  ];then
+    echo "   PID = PPID"
+else
+    echo "   PID ne PPID"
+fi
 
+echo "compare shell level..."
+if (return 0 2>/dev/null); then
+    echo "   sourced"
+    nstack=1
+else
+    echo "   not sourced"
+    nstack=2
+fi
 
-echo "$SHLVL"
-
-
- if (return 0 2>/dev/null); then
-     echo "sourced"
-     nstack=1
- else
-     echo "not sourced"
-     nstack=2
- fi
+echo "   assuming stack size is ${nstack}"
+echo "   shell level = $SHLVL"
 
 [[ $SHLVL -gt ${nstack} ]] &&
-  echo "called from parent" ||
-      echo "called directly"
+    echo "   called from parent" ||
+	echo "   called directly"
 
-echo "called by $(ps -o comm= $PPID)"
+echo "compare process name..."
+called_by=$(ps -o comm= $PPID)
+echo "   called by ${called_by}"
+echo -en "${white}"
+echo "   ----------------------"
+if [ "${called_by}" = "bash" ] || [ "${called_by}" = "SessionLeader" ]; then
+    echo "   envoked by shell"
+else
+    echo "   envoked by another process"
+fi
+echo "   ----------------------"
+echo -en "${NORMAL}"
+
+echo
+echo "compare pstree"
+pstree -Apu -H $$
+pstree -Apu -H $PPID
+
+pstree -Apu -H $$ | grep $$
+
 # print time at exit
 echo -en "${TAB}${PSDIR}$(basename $BASH_SOURCE)${NORMAL} "
 end_time=$(date +%s%N)
@@ -76,6 +97,6 @@ if command -v sec2elap &>/dev/null
 then
     echo -n "$(sec2elap $dT_sec | tr -d '\n')" 
 else
-    echo -n "elapsed time is ${dT_sec} sec"
+    echo -en "elapsed time is ${white}${dT_sec} sec${NORMAL}"
 fi
 echo " on $(date +"%a %b %-d at %-l:%M %p %Z")"
