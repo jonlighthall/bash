53d5f61db48ce350c0b9d0f14d6670108e4d5b33
Jon Lighthall <jon.lighthall@gmail.com>
2023-08-22 00:20:18 -0500

    working discriminator of child process

--- test/child.sh
+++ test/child.sh
@@ -2,7 +2,6 @@
 start_time=$(date +%s%N)
 # print source name at start
 echo
-echo "source----------------------"
 if (return 0 2>/dev/null); then
     RUN_TYPE="sourcing"
 else
@@ -15,50 +14,11 @@ src_name=$(readlink -f $BASH_SOURCE)
 if [ ! "$BASH_SOURCE" = "$src_name" ]; then
     echo -e "        ${VALID}link${NORMAL} = $src_name"
 fi
-echo -e "          \$0 = ${0}"
-
-echo "    \$0 running ${0##*/}"
 
 echo
-echo "parent----------------------"
-echo "compare shell/script to source..."
-if [ "${0}" = "$BASH_SOURCE"  ];then
-    echo -e "   \$0 same as \$BASH_SOURCE"
-    echo "   This script is called by another process."
-else
-    echo -e "   \$0 NOT same as \$BASH_SOURCE"
-    echo "   This script is not called by another process."
-fi
-
-echo "compare PID..."
-echo "          PID = $$"
-echo "   parent PID = $PPID"
-
-BASE_LVL=0
-echo "compare shell level..."
-if (return 0 2>/dev/null); then
-    echo -e "\x1b[33m   sourced\x1b[0m"
-    nstack=1
-
-else
-    echo -e "\x1b[34m   not sourced\x1b[0m"
-    nstack=2
-    BASE_LVL=$((BASE_LVL+1))
-fi
-
-echo "   assuming stack size is ${nstack}"
-echo "   shell level = $SHLVL"
-echo "BASE_LVL = $BASE_LVL"
-
-[[ $SHLVL -gt ${nstack} ]] &&
-    echo "   called from parent" ||
-	echo "   called directly"
-
 echo "compare process name..."
 called_by=$(ps -o comm= $PPID)
 echo "   called by ${called_by}"
-echo -en "${white}"
-echo "   ----------------------"
 if [ "${called_by}" = "bash" ] || [ "${called_by}" = "SessionLeader" ]; then
     echo -e "\x1b[0;36m   envoked by shell\x1b[0m"
 
@@ -66,8 +26,6 @@ else
     echo -e "\x1b[0;31m   envoked by another process\x1b[0m"
 
 fi
-echo "   ----------------------"
-echo -en "${NORMAL}"
 
 echo
 echo "compare pstree"
@@ -75,26 +33,92 @@ pstree -Apu | grep $$ | xargs
 pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/"
 pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//"
 SH_LEV=$(pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | grep -o "\-\-\-" | wc -l)
-echo "ps tree shell level $SH_LEV"
-echo -n "same as SHLVL? "
+echo "   ps tree shell level = $SH_LEV"
+#echo "           shell level = $SHLVL"
+#echo -n "   same as SHLVL? "
 if [ "$SHLVL" = "$SH_LEV" ]; then
+:
+    #   echo "yes"
+else
+  #  echo "no"
+    exit 1
+fi
+
+SHELL_NAME=${SHELL##*/}
+echo
+echo "compare with shell"
+echo "   shell is $SHELL_NAME"
+echo "-----------------------------------"
+pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g"
+PS_LEV=$(pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g" | grep -o "\-\-\-" | wc -l)
+
+pstree -Apu | grep $PPID | xargs | sed "s/\($PPID[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g"
+PPS_LEV=$(pstree -Apu | grep $PPID | xargs | sed "s/\($PPID[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g" | grep -o "\-\-\-" | wc -l)
+
+PSH_LEV=$((SH_LEV - PS_LEV))
+echo "-----------------------------------"
+echo "           shell level = $SHLVL"
+echo "         process level = $PS_LEV"
+echo "  parent process level = $PPS_LEV" 
+echo "    prompt shell level = $PSH_LEV"
+echo "-----------------------------------"
+
+if [ "$PPS_LEV" -eq 0 ]; then
     echo "yes"
+
+    # only works if sourced
+    if [ "${0}" = "$BASH_SOURCE"  ];then
+	echo -e "      \$0 same as \$BASH_SOURCE"
+	echo "      This script is called by another process."
+    else
+	echo -e "      \$0 NOT same as \$BASH_SOURCE"
+	echo "      This script is not called by another process."
+    fi
+
+    
 else
     echo "no"
 fi
-SHELL_NAME=${SHELL##*/}
-echo "shell is $SHELL_NAME"
-pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g"
-PS_LEV=$(pstree -Apu | grep $$ | xargs | sed "s/\($$[^)]*)\).*/\1/" | sed "s/^.*SessionLeader([0-9]*)//" | sed "s/\-\-\-$SHELL_NAME([^)]*)//g" | grep -o "\-\-\-" | wc -l)
-echo "process level $PS_LEV"
-PSH_LEV=$((SH_LEV - PS_LEV))
-echo "prompt shell level is $PSH_LEV"
+
+echo
+echo "compare shell level..."
+if (return 0 2>/dev/null); then
+    echo -e "\x1b[33m   sourced\x1b[0m"
+    echo "   compare shell/script to source..."
+    # only works if sourced
+    if [ "${0}" = "$BASH_SOURCE"  ];then
+	echo -e "      \$0 same as \$BASH_SOURCE"
+	echo "      This script is called by another process."
+    else
+	echo -e "      \$0 NOT same as \$BASH_SOURCE"
+	echo "      This script is not called by another process."
+    fi
+    BASE_LVL=0
+else
+    echo -e "\x1b[34m   not sourced\x1b[0m"
+    BASE_LVL=1
+fi
+
+echo "BASE_LVL = $BASE_LVL"
+
+[[ $SHLVL -gt ${nstack} ]] &&
+    echo "   called from parent" ||
+	echo "   called directly"
 
 echo "BASE_LVL = $BASE_LVL"
 echo "process level $PS_LEV"
-echo -n "PS lev > $BASE_LVL " 
+echo "PS lev > $BASE_LVL " 
 
-if [ "$PS_LEV" -gt "$BASE_LVL" ]; then
+echo -n "$SHLVL  -gt $PS_LEV "
+
+#if [ "$PS_LEV"  ]; then
+#if [ "$PS_LEV" -gt "$BASE_LVL" ]; then
+
+
+
+if [ "$PS_LEV" -gt 1 ] || ( [ "$PS_LEV" -gt "$PPS_LEV" ] ) || ( [ "$SH_LEV" -gt "$PSH_LEV" ] && [ "$PS_LEV" -gt 1 ] ); then
+
+    
     echo true
     echo -e "\x1b[0;35m$(basename $BASH_SOURCE) was envoked by another process\x1b[0m"
 else
