54b9654804f02d6fb9d4030743d7b89c3712a0af
Jon Lighthall <jon.lighthall@gmail.com>
2023-09-01 09:05:16 -0500

    add color print from Aug 25

---
+++ test/col16.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+case $TERM in
+    xterm*)
+        TERM=xterm-256color
+        ;;
+    linux*)
+        TERM=linux-16color
+        ;;
+esac
+export TERM
+for p in $(seq 0 15)
+do
+    tput setab $p
+    for q in $(seq 0 15)
+    do
+        tput setaf $q
+        printf '%x%x' $p $q
+    done
+    tput sgr0
+    printf '\n'
+done
