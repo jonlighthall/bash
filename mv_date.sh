120156114b598b4cccbd7928fb123263abdf75a7
Jon Lighthall <jon.lighthall@gmail.com>
2023-09-02 17:02:23 -0500

    mv_date: update formatting, remove timestamp

--- mv_date.sh
+++ mv_date.sh
@@ -17,48 +17,49 @@ else
     file_in=$(readlink -f $1)
     echo "argument 1: $1"
     TAB+=${fTAB:='   '}
-    echo -n "${TAB}input file ${file_in}... "
+    echo -n "${TAB} input path: ${file_in}... "
     if [ -f ${file_in} ]; then
 	echo "exits"
 
 	# parse input
 	in_dir=$(dirname $file_in)
-	echo "${TAB}input dir = $in_dir"
+	echo "${TAB}  input dir: $in_dir"
 	in_fname=$(basename $file_in)
-	echo "${TAB}input file = $in_fname"
+	echo "${TAB} input file: $in_fname"
 	in_base="${in_fname%.*}"
-	echo "${TAB}base name = $in_base"
-	TAB+=${fTAB}
+	echo "${TAB}  base name: $in_base"
 	if [[ $in_fname == *"."* ]]; then
-	    echo "${TAB}fname contains dots"
 	    ext=".${in_fname##*.}"
 	else
-	    echo "${TAB}fname does not contains dots, no extension will be used"
 	    ext=""
 	fi
-	TAB=${TAB%$fTAB}
+	echo -n "${TAB}   ext name: ${ext}"
+	if [ ${#ext} -eq 0 ]; then
+	    echo "EMPTY"
+	else
+	    echo
+	fi
 
 	# set default output file name to match input
 	out_base="${in_base}"
 	file_out="${in_dir}/${out_base}${ext}"
 
-	TAB=${TAB%$fTAB}
-	echo "file specification = $file_out"
+	echo "${TAB}output file: $file_out"
 
 	# check if input and output are the same file
 	echo -n " input file ${file_in} is... "
 	while [ ${file_in} -ef ${file_out} ]; do
-            echo "the same file as ${file_out}"
+            echo -e "\n${TAB}the same file as output file ${file_out}"
             echo -n "${TAB}renaming output... "
 	    file_out=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
 	    echo ${file_out}
 	done
-	echo "uniquely named"
+	echo "${TAB}uniquely named"
 
 	# check if output exists
-	echo -n "output file ${file_out}... "
+	echo "output file ${file_out}... "
 	if [ -f ${file_out} ]; then
-            echo "exists"
+            echo "${TAB}exists"
 	    while [ -f ${file_out} ]; do
 		echo "${file_out##*/} exists"
 		echo -n "${TAB}renaming output... "
@@ -67,12 +68,11 @@ else
 	    done
 	    echo "${TAB}unique file name found"
 	else
-	    echo "uniquely named"
+	    echo "${TAB}does not exist (uniquely named)"
 	fi
     else
-	echo "does not exit"
+	echo "does not exist"
+	echo "${TAB}exiting..."
 	exit 1
     fi
 fi
-# print time at exit
-echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
