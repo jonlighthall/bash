379c0a5091ecefc704cdacf70fc7dbd6634d75e1
Jon Lighthall <jon.lighthall@gmail.com>
2023-09-12 10:29:59 -0500

    add quotes to all file names

--- mv_date.sh
+++ mv_date.sh
@@ -18,13 +18,13 @@ else
     echo "argument 1: $1"
     TAB+=${fTAB:='   '}
     echo -n "${TAB} input path: ${file_in}... "
-    if [ -f ${file_in} ]; then
+    if [ -f "${file_in}" ]; then
 	echo "exits"
 
 	# parse input
-	in_dir=$(dirname $file_in)
+	in_dir=$(dirname "${file_in}")
 	echo "${TAB}  input dir: $in_dir"
-	in_fname=$(basename $file_in)
+	in_fname=$(basename "${file_in}")
 	echo "${TAB} input file: $in_fname"
 	in_base="${in_fname%.*}"
 	echo "${TAB}  base name: $in_base"
@@ -46,21 +46,21 @@ else
 
 	# check if input and output are the same file
         echo -e "output file ${file_out} is ..."
-	while [ ${file_in} -ef ${file_out} ]; do
+	while [ "${file_in}" -ef "${file_out}" ]; do
 	    echo "${TAB}the same file as input file ${file_in}"
             echo -n "${TAB}renaming output... "
 	    # NB: don't rename any existing files; change the ouput file name to something unique
-	    file_out=${in_dir}/${out_base}_$(date -r ${file_out} +'%Y-%m-%d-t%H%M%S')${ext}
+	    file_out=${in_dir}/${out_base}_$(date -r "${file_out}" +'%Y-%m-%d-t%H%M%S')${ext}
 	    echo ${file_out}
 	done
 	echo "${TAB}uniquely named"
 
 	# check if output exists
 	echo "output file ${file_out}... "
-	if [ -f ${file_out} ]; then
+	if [ -f "${file_out}" ]; then
             echo "${TAB}exists"
 	    echo -n "${TAB}waiting for new time stamp... "
-	    while [ -f ${file_out} ]; do
+	    while [ -f "${file_out}" ]; do
 		# NB: don't rename any existing files; change the ouput file name to something
 		# unique
 		file_out=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
@@ -80,4 +80,4 @@ else
 fi
 
 # now move file
-mv -nv ${file_in} ${file_out}
+mv -nv "${file_in}" "${file_out}"
