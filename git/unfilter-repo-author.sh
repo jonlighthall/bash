d55a49edb53c30568d229af9a11e86a8c73a60ee
Jon Lighthall <jon.lighthall@gmail.com>
2023-09-29 12:15:10 -0500

    add read url file to unfilter list

--- git/unfilter-repo-author.sh
+++ git/unfilter-repo-author.sh
@@ -1,8 +1,16 @@
 git filter-repo $@ --partial --commit-callback '
     correct_name = b"Jon Lighthall"
     auth_list = [b"jlighthall@fsu.edu",b"jon.lighthall@gmail.com",b"lighthall@lsu.edu"]
-    auth_list.append(b"jonathan.c.lighthall@")
-    correct_email = b"jonathan.lighthall@"
+    text_file = open(os.path.expanduser("~/utils/bash/git/url.txt"), "r")
+    url = text_file.read()
+    text_file.close()
+    email_str="jonathan.c.lighthall@"+url.strip()
+    email_bin=email_str.encode("ascii")	
+    auth_list.append(email_bin)
+    email_str="jonathan.lighthall@"+url.strip()
+    email_bin=email_str.encode("ascii")	
+    auth_list.append(email_bin)
+    correct_email = email_bin
     if commit.author_email in auth_list:
         commit.author_email = correct_email
         if commit.author_name != correct_name:
