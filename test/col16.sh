56e53c697cd9b15f592ef13ec230cdbec0723a76
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 17:56:50 -0600

    update var names

--- test/col16.sh
+++ test/col16.sh
@@ -8,13 +8,13 @@ case $TERM in
         ;;
 esac
 export TERM
-declare -i qmax=15
-declare -i pmax=${qmax}
-for q in $(seq 0 ${qmax}); do
-    tput setaf $q
-	for p in $(seq 0 ${pmax}); do
-		tput setab $p
-		printf ' b=%2d f=%2d ' $p $q
+declare -i fore_max=15
+declare -i back_max=${fore_max}
+for fore in $(seq 0 ${fore_max}); do
+    tput setaf $fore
+	for back in $(seq 0 ${back_max}); do
+		tput setab $back
+		printf ' b=%2d f=%2d ' $back $fore
 	done
 	tput sgr0
 	printf '\n'
