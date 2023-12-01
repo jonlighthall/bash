33cf1f10c2b2568cedb3e17ff73004cfa7a8964c
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 11:16:35 -0600

    add formatting

--- add_path.sh
+++ add_path.sh
@@ -5,23 +5,23 @@
 # NB: remember to 'source' this script, don't just execute it
 #
 # Path additions
-for ADDPATH in "$@"
-do
+for ADDPATH in "$@"; do
     echo -n ${ADDPATH}
-    if [ -d "${ADDPATH}" ] ; then
-	echo " found"
-	ABSPATH=$(cd $ADDPATH; \pwd)
-	echo -n ${ABSPATH}
+    if [ -d "${ADDPATH}" ]; then
+        echo " found"
+        ABSPATH=$(cd $ADDPATH; \pwd)
+        echo -n ${ABSPATH}
     else
-	echo " not found"
+        echo " not found"
     fi
-    if [[ "$PATH" != *"${ABSPATH}:"*  ]]; then
-	export PATH=$ABSPATH:$PATH
-	echo " added to PATH"
+    if [[ "$PATH" != *"${ABSPATH}:"* ]]; then
+        export PATH=$ABSPATH:$PATH
+        echo " added to PATH"
     else
-	echo " already in PATH"
+        echo " already in PATH"
     fi
 done
 echo "done"
+
 # print time at exit
 echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
