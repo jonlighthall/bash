7efa3702c6bc166e7d2f036de4e608b164fe6921
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 11:15:14 -0600

    add comments, formatting

--- welcome.sh
+++ welcome.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -u
+# Purpose: Display a welcome message, including the host name, on remote host after login
+
+# define welcome message
 msg=$(echo " Welcome to $(hostname -s) ")
+# create a line of dashes the same width as $msg
 ln=$(for ((i = 1; i <= ${#msg}; i++)); do echo -n "-"; done)
+# display welcome message
 echo -e "\033[0;33m$ln\n$msg\n$ln\033[0m"
 exit
