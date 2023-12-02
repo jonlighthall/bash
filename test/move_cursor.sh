0d75f3bec6ae59e287b1df3d21c4492039f75707
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 18:51:45 -0600

    add function, set x

--- test/move_cursor.sh
+++ test/move_cursor.sh
@@ -1,20 +1,29 @@
 #!/bin/bash -u
+function cursor_pos() {
+	# get the cursor position
+	echo -en "\E[6n"
+	read -sdR CURPOS
+	# parse the cursor position
+	CURPOS=${CURPOS#*[}
+	x_pos=${CURPOS#*;}
+	y_pos=${CURPOS%;*}
+	# print the cursor position
+	echo -e "\nposition is x=$x_pos y=$y_pos"
+}
+
 clear -x
 echo -en "\x1B[1;34mhere\x1B[0m"
 sleep 1
-echo -en "\E[6n"
-read -sdR CURPOS
-CURPOS=${CURPOS#*[}
-cx=${CURPOS#*;}
-cy=${CURPOS%;*}
-echo "position is x=$cx y=$cy"
-ls -la
+cursor_pos
+set -x
+ls -la --color=always
 sleep 1
+set +x
 echo -en "\E[6n"
 read -sdR CURPOS
 CURPOS=${CURPOS#*[}
 cx1=${CURPOS#*;}
 cy1=${CURPOS%;*}
-echo -en "\x1B[${cy};${cx}H \033[1;31mthere $cx $cy\033[0m"
+echo -en "\x1B[${y_pos};${x_pos}H \033[1;31mthere $x_pos $y_pos\033[0m"
 sleep 1
 echo -e "\x1B[${cy1};${cx1}H\033[1;35manywhere $cx1 $cy1\033[0m"
