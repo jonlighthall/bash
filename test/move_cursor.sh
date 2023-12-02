3ba40aa5cc63aacb923bc3619c8ee372bec7e34a
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 18:46:37 -0600

    add sleep

--- test/move_cursor.sh
+++ test/move_cursor.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -u
 clear -x
 echo -en "\x1B[1;34mhere\x1B[0m"
+sleep 1
 echo -en "\E[6n"
 read -sdR CURPOS
 CURPOS=${CURPOS#*[}
-echo "${CURPOS}"
 cx=${CURPOS#*;}
 cy=${CURPOS%;*}
 echo "position is x=$cx y=$cy"
 ls -la
+sleep 1
 echo -en "\E[6n"
 read -sdR CURPOS
 CURPOS=${CURPOS#*[}
 cx1=${CURPOS#*;}
 cy1=${CURPOS%;*}
-echo -e "\x1B[${cy};${cx}H \033[1;31mthere $cx $cy\033[0m"
+echo -en "\x1B[${cy};${cx}H \033[1;31mthere $cx $cy\033[0m"
+sleep 1
 echo -e "\x1B[${cy1};${cx1}H\033[1;35manywhere $cx1 $cy1\033[0m"
