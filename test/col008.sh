08e917204cd94ff42dd665accccbcac69eb312a1
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-01 18:13:23 -0600

    add modes, skip 8

--- test/col008.sh
+++ test/col008.sh
@@ -8,12 +8,23 @@ case $TERM in
         ;;
 esac
 export TERM
-declare -i fore_max=8
+declare -i fore_max=9
 declare -i back_max=${fore_max}
-for fore in $(seq 0 ${fore_max}); do
-	for back in $(seq 0 ${back_max}); do
-		echo -ne "\E[3${fore};4${back}m"
-		printf ' b=3%d f=4%d ' $back $fore
+declare -i mode_max=9
+for mode in $(seq 0 ${mode_max}); do
+	echo "mode = ${mode}"
+	for fore in $(seq 0 ${fore_max}); do
+		if [ $fore -eq 8 ]; then
+			continue
+		fi			
+		for back in $(seq 0 ${back_max}); do
+			if [ $back -eq 8 ]; then
+				continue
+			fi			
+			echo -ne "\E[${mode};3${fore};4${back}m"
+			printf ' %d;3%d;4%d ' $mode $fore $back
+		done
+		echo -e "\E[m"
 	done
 	echo -e "\E[m"
 done
