910a77adaf520398b6e78a801273b2a6c4bc72f3
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-14 12:40:28 -0600

    fix date to age

--- date2age.sh
+++ date2age.sh
@@ -1,2 +1,15 @@
 #!/bin/bash -u
-echo $(($(date +%s) - $1))
+if [ $# -eq 0 ]; then
+	echo "Please provide an input file"
+	exit 1
+fi
+echo "now is $(date)"
+declare -i A=$(date +%s)
+echo "age is $A"
+echo "file date is $(date -r "$1")"
+declare -i B=$(date -r $1 +%s)
+echo "file age is $B"
+declare -i C=$(( A - B ))
+echo -n "file age is $C seconds or "
+sec2elap $C 
+
