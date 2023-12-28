f6a02158117e11586a5eea23c67bda2880af8abc
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-27 20:07:25 -0600

    add decimals

--- test/test_sec2elap.sh
+++ test/test_sec2elap.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -u
 # program to test formatting of sec2elap over different time epochs
-for n in {1..8}
-do
-    echo $n
-    echo $((10**n))
-    bash sec2elap $((10**$n))
+for n in {0..8}; do
+    echo -n "10^$n = "
+    declare -i i=$((10**$n))
+	echo "$i"
+	((i--))
+	dec="."
+	for m in {1..8}; do 
+		echo -n "  $i, $m: "
+		dec+="$m"
+		declare j=$(echo "$i$dec")
+		echo "$j"
+		bash sec2elap $j
+	done
 done
 # print time at exit
 echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
