911e546b1fd052da591ce4ee1bd472f49855f03e
Jon Lighthall <jon.lighthall@gmail.com>
2023-12-30 14:07:04 -0600

    add formatting to xtest

--- xtest.sh
+++ xtest.sh
@@ -7,12 +7,12 @@ list_test="xeyes xclock xcalc xman xlogo xterm"
 list_found=''
 
 for prog in $list_test; do
-    echo -e "opening $prog... \c"
+    echo -e "opening $prog... \r\E[18C\c"
     $prog 2>/dev/null &
     if ! command -v $prog &>/dev/null; then
-        echo "not found"
+        echo -e "\E[33mnot found\E[0m"
     else
-        echo "OK"
+        echo -e "\E[32mOK\E[0m"
         list_found="$list_found $prog"
     fi
 done
@@ -23,9 +23,15 @@ if [ -z "${list_found}" ]; then
 fi
 
 # close windows
-read -n 1 -s -r -p "Press any key to continue"
+read -n 1 -s -r -p $'\E[32m> \E[0mPress any key to continue'
 echo
 for prog in $list_found; do
-    echo "closing $prog..."
-    pkill $prog 2>/dev/null &
+    echo -e "closing $prog...\r\E[18C\c"
+    pkill $prog 2>/dev/null
+	RETVAL=$?
+	if [ $RETVAL = 0 ]; then
+     echo -e "\E[90mOK\E[0m"
+	else
+		echo -e "\E[31mFAIL\E[0m"
+	fi
 done
