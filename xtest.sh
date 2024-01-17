66f4653eb32ec8cc405a793b90a6d8f64b7e7f97
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-17 14:18:54 -0600

    use which to find, separate open and check

--- xtest.sh
+++ xtest.sh
@@ -4,17 +4,19 @@ echo "${0##*/}"
 # test X11
 export LC_ALL=C
 list_test="xeyes xclock xcalc xman xlogo xterm"
-list_found=''
 
+# find programs
+list_found=''
 for prog in $list_test; do
-    echo -e "opening $prog... \r\E[18C\c"
-    $prog 2>/dev/null &
-    if ! command -v $prog &>/dev/null; then
-        echo -e "\E[33mnot found\E[0m"
-    else
-        echo -e "\E[32mOK\E[0m"
-        list_found="$list_found $prog"
-    fi
+    echo -e "locating $prog... \r\E[19C\c"
+	which $prog
+	RETVAL=$?
+	if [ $RETVAL = 0 ]; then
+		#		echo -e "   \E[32mOK (found)\E[0m"
+		list_found="$list_found $prog"
+	else
+		echo -e "\E[31mFAIL \E[90mRETVAL=$RETVAL\E[0m"
+	fi
 done
 
 if [ -z "${list_found}" ]; then
@@ -22,16 +24,70 @@ if [ -z "${list_found}" ]; then
     exit
 fi
 
+echo
+# open programs
+list_open=''
+for prog in $list_found; do
+    echo -e " opening $prog... \r\E[19C\c"
+	#		STDERR=$($prog & 2>&1 >/dev/null)
+	# if [ ${#STDERR} -eq 0]; then
+	# 	echo "no error"
+	# else
+	# 	echo "error"
+	# fi
+
+	#		$prog &
+	$prog 2>/dev/null & 
+	RETVAL=$?
+	#		echo "   RETVAL=$RETVAL"
+	PID=$!
+	#		echo "   PID=$PID"
+	#		echo -n "   ps: "
+	#		ps | grep "$PID"
+
+	if [ $RETVAL = 0 ]; then
+		echo -e "\E[32mOK (open)\E[0m"
+		list_open="$list_open $prog"
+	else
+		echo -e "\E[31mFAIL \E[90mRETVAL=$RETVAL\E[0m"
+	fi		
+done
+
+if [ -z "${list_open}" ]; then
+    echo "FAIL: no X-programs opened!"
+    exit
+fi
+
+# check if programs are running
+echo
+list_run=''
+for prog in $list_found; do
+    echo -e "checking $prog... \r\E[19C\c"
+	ps | grep "$prog" >/dev/null
+	RETVAL=$?
+	if [ $RETVAL = 0 ]; then
+		echo -e "\E[32mOK (running)\E[0m"
+		list_run="$list_run $prog"
+	else
+		echo -e "\E[31mFAIL \E[90mRETVAL=$RETVAL\E[0m"
+	fi
+done
+
+if [ -z "${list_run}" ]; then
+    echo "FAIL: no X-programs running!"
+    exit
+fi
+
 # close windows
 read -n 1 -s -r -p $'\E[32m> \E[0mPress any key to continue'
 echo
-for prog in $list_found; do
-    echo -e "closing $prog...\r\E[18C\c"
-    pkill $prog 2>/dev/null
+for prog in $list_run; do
+    echo -e " closing $prog...\r\E[19C\c"
+    pkill $prog
 	RETVAL=$?
 	if [ $RETVAL = 0 ]; then
-     echo -e "\E[90mOK\E[0m"
+		echo -e "\E[90mOK\E[0m"
 	else
-		echo -e "\E[31mFAIL\E[0m"
+		echo -e "\E[31mFAIL \E[90mRETVAL=$RETVAL\E[0m"
 	fi
 done
