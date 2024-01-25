28b481f2a1af9ebc6f8ecc5ea2904cc54ef0224e
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-25 14:36:59 -0600

    add decho to ns2s, add test

---
+++ test/test_ns2sec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -u
+# program to test formatting of sec2elap over different time epochs
+for n in {0..8}; do
+    echo -n "10^$n = "
+    declare -i i=$((10**$n))
+	echo "$i"
+	((i--))
+	dec="."
+	for m in {1..8}; do 
+		echo -n "  $i, $m: "
+		dec+="$m"
+		declare j=$(echo "$i$dec")
+		echo "$j"
+		if [ $m -ge $n ]; then 
+			bash ../ns2s.sh $j
+		fi
+	done
+done
+# print time at exit
+echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
