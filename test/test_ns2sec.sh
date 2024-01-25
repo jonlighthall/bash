877e0375cbab8e4f35a4ad6c904e497d7d2d7433
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-25 15:10:29 -0600

    fix leading zero decimal place

--- test/test_ns2sec.sh
+++ test/test_ns2sec.sh
@@ -1,19 +1,19 @@
-#!/bin/bash -u
+#!/bin/bash -ueE
 # program to test formatting of sec2elap over different time epochs
-for n in {0..8}; do
+for n in {0..10}; do
     echo -n "10^$n = "
     declare -i i=$((10**$n))
 	echo "$i"
 	((i--))
 	dec="."
-	for m in {1..8}; do 
+	for m in {0..2}; do 
 		echo -n "  $i, $m: "
 		dec+="$m"
 		declare j=$(echo "$i$dec")
 		echo "$j"
-		if [ $m -ge $n ]; then 
-			bash ../ns2s.sh $j
-		fi
+#		if [ $m -ge $n ]; then 
+		../ns2s.sh $j
+#		fi
 	done
 done
 # print time at exit
