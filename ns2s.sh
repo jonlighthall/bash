7b995a0711f8d69a7944e247aa6b0e001ab43e24
Jon Lighthall <jon.lighthall@gmail.com>
2024-01-25 14:33:57 -0600

    fix rounding

--- ns2s.sh
+++ ns2s.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -u
 
-trap "echo -e '\x1B[11G BASH: $(sec2elap $SECONDS)'" EXIT
-
 if [ $# -eq 0 ]; then
 	echo "NULL"
 else
@@ -25,15 +23,17 @@ else
 		declare -i nd=${#deci}
 		declare frac=".${deci}"
 	else
-		declare -i deci=''
+		declare deci=''
 		declare -i nd=0
 		declare frac=''
 	fi
 	echo -e "\x1B[${ln}G and $nd decimal places"
 
-	echo "decimals: $deci"
-	echo "number of decimails: $nd"
-	echo "fractional part: $frac"
+	if [ $nd -gt 0 ]; then
+		echo "decimals: $deci"
+		echo "number of decimails: $nd"
+		echo "fractional part: $frac"
+	fi
 
 	# pad timestamp with leading zeros
 	if [ $nw -lt $nd_max ]; then
@@ -61,25 +61,55 @@ else
 
 	pad0f="${pad0}${frac}"	
 	declare -i nl0=${#pad0f}
-	echo "new lenght: $nl0"
+	echo "padded lenght: $nl0"
 	
-	echo "zero-padded: $pad0f"		
+	echo "zero-padded: $pad0f ns"		
 fi
 
 # format timestamp in s
 if [ $nw -gt $nd_max ]; then
-	echo "greater than 1 s"
 	ni=$(($nw-$nd_max))
-	wholns=${pad0:0:$ni}.${pad0:$ni}
+	declare sdec=${pad0:$ni}
+	if [ $sdec -gt 0 ]; then 
+		echo "greater than 1 s"
+	else
+		echo "equal to 1 s"
+	fi
+	wholns=${pad0:0:$ni}.${sdec}
 else
-	echo "less than or equal to 1 s"
+	echo "less than 1 s"
 	wholns="0.${pad0}"
 fi
 echo "   whole ns: $wholns"
+if [ $nd -gt 0 ]; then
+	echo "    deci ns: $deci"
+fi
 fracns="${wholns}${deci}"
-echo "decimalized: $fracns "
+echo "decimalized: $fracns s"
 
 # round timestamp to nearest second
 fmt="%.0f"			
 declare -i whols=$(printf "$fmt" ${fracns})
-echo "integerized: $whols "
+echo "integerized: $whols s"
+
+if [[ "$fracns" = *"."* ]]; then
+	# determine number of decimal places
+	declare  decis=${fracns#*.}
+	declare  nds=${#decis}
+else
+	declare -i decis=''
+	declare -i nds=0
+fi
+echo -e "\x1B[${ln}G and $nds decimal places"
+
+echo "remainder: 0.$decis"
+
+if [ ${decis} -gt 0 ]; then
+	echo "round up"
+	declare rsec=$((whols+1))
+else
+	echo "no change"
+	declare rsec=$whols
+fi
+
+echo "    rounded: $rsec s"
