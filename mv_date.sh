def8744f486a8761080385f7ee4ed6ecd2cd9638
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-08 11:30:30 -0600

    remove umbrella 'else'

--- mv_date.sh
+++ mv_date.sh
@@ -10,73 +10,72 @@
 if [ $# -eq 0 ]; then
 	echo "Please provide an input file"
 	exit 1
-else
-	echo "number of arguments = $#"
+fi
+echo "number of arguments = $#"
 
-	# set file names
-	in_file=$(readlink -f $1)
-	echo "argument 1: $1"
-	TAB+=${fTAB:='   '}
-	echo -n "${TAB} input path: ${in_file}... "
-	if [ -f "${in_file}" ]; then
-		echo "exits"
+# set file names
+in_file=$(readlink -f $1)
+echo "argument 1: $1"
+TAB+=${fTAB:='   '}
+echo -n "${TAB} input path: ${in_file}... "
+if [ -f "${in_file}" ]; then
+	echo "exits"
 
-		# parse input
-		in_dir=$(dirname "${in_file}")
-		echo "${TAB}  input dir: $in_dir"
-		in_fname=$(basename "${in_file}")
-		echo "${TAB} input file: $in_fname"
-		in_base="${in_fname%.*}"
-		echo "${TAB}  base name: $in_base"
-		if [[ $in_fname == *"."* ]]; then
-			ext=".${in_fname##*.}"
-		else
-			ext=""
-		fi
-		echo -n "${TAB}   ext name: ${ext}"
-		if [ ${#ext} -eq 0 ]; then
-			echo "EMPTY"
-		else
-			echo
-		fi
-
-		# set default output file name to match input
-		out_base="${in_base}"
-		out_file="${in_dir}/${out_base}${ext}"
-
-		# check if input and output are the same file
-		echo -e "output file ${out_file} is ..."
-		while [ "${in_file}" -ef "${out_file}" ]; do
-			echo "${TAB}the same file as input file ${in_file}"
-			echo -n "${TAB}renaming output... "
-			# NB: don't rename any existing files; change the ouput file name to something unique
-			out_file=${in_dir}/${out_base}_$(date -r "${out_file}" +'%Y-%m-%d-t%H%M%S')${ext}
-			echo ${out_file}
-		done
-		echo "${TAB}uniquely named"
-
-		# check if output exists
-		echo "output file ${out_file}... "
-		if [ -f "${out_file}" ]; then
-			echo "${TAB}exists"
-			echo -n "${TAB}waiting for new time stamp... "
-			while [ -f "${out_file}" ]; do
-				# NB: don't rename any existing files; change the ouput file name to something
-				# unique
-				out_file=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
-			done
-			echo "done"
-			echo "${TAB}unique file name found"
-			out_file=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
-			echo "output file ${out_file}"
-		else
-			echo "${TAB}does not exist (uniquely named)"
-		fi
+	# parse input
+	in_dir=$(dirname "${in_file}")
+	echo "${TAB}  input dir: $in_dir"
+	in_fname=$(basename "${in_file}")
+	echo "${TAB} input file: $in_fname"
+	in_base="${in_fname%.*}"
+	echo "${TAB}  base name: $in_base"
+	if [[ $in_fname == *"."* ]]; then
+		ext=".${in_fname##*.}"
 	else
-		echo "is not a file"
-		echo "${TAB}exiting..."
-		exit 1
+		ext=""
 	fi
+	echo -n "${TAB}   ext name: ${ext}"
+	if [ ${#ext} -eq 0 ]; then
+		echo "EMPTY"
+	else
+		echo
+	fi
+
+	# set default output file name to match input
+	out_base="${in_base}"
+	out_file="${in_dir}/${out_base}${ext}"
+
+	# check if input and output are the same file
+	echo -e "output file ${out_file} is ..."
+	while [ "${in_file}" -ef "${out_file}" ]; do
+		echo "${TAB}the same file as input file ${in_file}"
+		echo -n "${TAB}renaming output... "
+		# NB: don't rename any existing files; change the ouput file name to something unique
+		out_file=${in_dir}/${out_base}_$(date -r "${out_file}" +'%Y-%m-%d-t%H%M%S')${ext}
+		echo ${out_file}
+	done
+	echo "${TAB}uniquely named"
+
+	# check if output exists
+	echo "output file ${out_file}... "
+	if [ -f "${out_file}" ]; then
+		echo "${TAB}exists"
+		echo -n "${TAB}waiting for new time stamp... "
+		while [ -f "${out_file}" ]; do
+			# NB: don't rename any existing files; change the ouput file name to something
+			# unique
+			out_file=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
+		done
+		echo "done"
+		echo "${TAB}unique file name found"
+		out_file=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
+		echo "output file ${out_file}"
+	else
+		echo "${TAB}does not exist (uniquely named)"
+	fi
+else
+	echo "is not a file"
+	echo "${TAB}exiting..."
+	exit 1
 fi
 
 # now move file
