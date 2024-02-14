0a199c09bcd14045db8b3931dfd443dd990bbe6a
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-14 11:29:05 -0600

    unify add path and set path: add comments, checks, and timestamp

--- set_path.sh
+++ set_path.sh
@@ -8,15 +8,24 @@ for ADDPATH in \
 	${PWD%/*}/ \
 	${PWD%/*/*}/; do
 	echo -n ${ADDPATH}
+	# check if path exists
+	if [ -d "${ADDPATH}" ]; then
+		echo " found"
+		ABSPATH=$(cd $ADDPATH; \pwd)
+		echo -n ${ABSPATH}
+	else
+		echo " not found"
+		continue
+	fi
+	# prepend path to PATH
 	if [[ "$PATH" != *"${ADDPATH}:"* ]]; then
-		if [ -d "${ADDPATH}" ]; then
-			export PATH=$ADDPATH:$PATH
-			echo " added to PATH"
-		else
-			echo " not found"
-		fi
+		export PATH=$ADDPATH:$PATH
+		echo " added to PATH"
 	else
 		echo " already in PATH"
 	fi
 done
 echo "done"
+
+# print time at exit
+echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
