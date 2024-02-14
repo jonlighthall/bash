193fcb77346347814abdc246362d5db905d74962
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-14 11:29:05 -0600

    unify add path and set path: add comments, checks, and timestamp

--- add_path.sh
+++ add_path.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -u
-# add the directories passed to the command as arguments to the start
-# of the PATH environmental variable
+# add_path.sh - prepends arguments to PATH. Add the directories passed to the
+# command as arguments to the start of the PATH environmental variable.
 #
 # NB: remember to 'source' this script, don't just execute it
 #
 # Path additions
 for ADDPATH in "$@"; do
     echo -n ${ADDPATH}
+# check if path exists
     if [ -d "${ADDPATH}" ]; then
         echo " found"
         ABSPATH=$(cd $ADDPATH; \pwd)
         echo -n ${ABSPATH}
     else
         echo " not found"
+        continue
     fi
-    if [[ "$PATH" != *"${ABSPATH}:"* ]]; then
-        export PATH=$ABSPATH:$PATH
+    # prepend path to PATH
+	if [[ "$PATH" != *"${ADDPATH}:"* ]]; then
+        export PATH=$ADDPATH:$PATH
         echo " added to PATH"
     else
         echo " already in PATH"
