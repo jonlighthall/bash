e471c50c0f7b36b5deb5e261ada94ef0c9804941
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-14 10:50:15 -0600

    add comments, declare's

--- test/print_tab.sh
+++ test/print_tab.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -u
-yellow='\E[0;33m'
-  GOOD='\E[0;32m' # green
-NORMAL='\E[0m'    # reset
- space='\E[30;106m'
 
-# set tab
-thisTAB='   '
+# define colors
+declare -r yellow='\E[0;33m' # yellow
+declare -r   GOOD='\E[0;32m' # green
+declare -r NORMAL='\E[0m'    # reset
+declare -r  space='\E[30;106m' # highlight white space
+
+# set lcoal tab
+declare -r thisTAB='   '
+
+# check tabs
 for var in TAB fTAB
 do
 	echo -n "${thisTAB}$var "
