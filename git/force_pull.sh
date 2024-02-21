2a58283306e2e3ce03c24e137099a2b4b6a7efcc
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-21 09:34:34 -0600

    unify force pull and update: add traps to pull; fix remote tracking branch definition, add formatting

--- git/force_pull.sh
+++ git/force_pull.sh
@@ -40,18 +40,88 @@ if [ -e $fpretty ]; then
 	source $fpretty
 fi
 
+# define traps
+function set_traps() {
+	decho -e "${magenta}\E[7mset traps${NORMAL}"
+	decho "setting shell options..."
+	if (return 0 2>/dev/null); then
+		decho -e "${magenta}\E[7mreturn flags${NORMAL}"
+		#		set -TE +e
+	else
+		decho -e "${magenta}\E[7mexit flags${NORMAL}"
+		set -e
+	fi
+	set -E
+	decho "the following traps are saved"
+	if [ -z "${save_traps+default}" ]; then
+		decho "${fTAB}none"
+
+		decho "setting traps..."
+		trap 'print_error $LINENO $? $BASH_COMMAND' ERR
+		trap print_exit EXIT
+		trap 'echo -e "${yellow}RETURN${NORMAL}: ${0##*/} $LINENO $? $BASH_COMMAND"' RETURN
+
+	else
+		decho "${save_traps}" | sed "s/^/${fTAB}/"
+		decho "setting saved traps..."
+		eval $(echo "${save_traps}" | sed "s/$/;/g")
+
+		#eval $(echo '${save_traps}')
+	fi
+	decho "on set trap retrun, the following traps are set"
+	if [ -z "$(trap -p)" ]; then
+		decho "${fTAB}none"
+		exit
+	else
+		decho $(trap -p | sed "s/^/${fTAB}/")
+	fi
+}
+
+function unset_traps() {
+	decho -e "${cyan}\E[7mun-set traps${NORMAL}"
+	decho "setting shell options..."
+	#	set +eET
+	set +eE
+
+	decho "the current traps are set"
+
+	if [ -z "$(trap -p)" ]; then
+		decho "${fTAB}none"
+	else
+		decho $(trap -p | sed "s/^/${fTAB}/")
+		# save traps
+		save_traps=$(trap -p | sed 's/-- //g')
+
+		if [ ! -z "${save_traps}" ]; then
+			decho "the current traps are saved"
+			decho "${save_traps}" | sed "s/^/${fTAB}/"
+		fi
+
+		trap - ERR
+		trap - EXIT
+		trap - RETURN
+
+	fi
+
+	decho "on unset trap retrun, the following traps are set"
+	if [ -z $(trap -p) ]; then
+		decho "${fTAB}none"
+	else
+		decho $(trap -p)
+		exit
+	fi
+}
+
+set_traps
+
 # determine if script is being sourced or executed and add conditional behavior
 if (return 0 2>/dev/null); then
-    RUN_TYPE="sourcing"
-    set -TE +e
-    trap 'echo -en "${yellow}RETURN${NORMAL}: ${BASH_SOURCE##*/} "' RETURN
+	RUN_TYPE="sourcing"
+	set -TE +e
 else
-    RUN_TYPE="executing"
-    # exit on errors
-    set -eE
-    trap 'print_error $LINENO $? $BASH_COMMAND' ERR
-    # print time at exit
-    trap print_exit EXIT
+	RUN_TYPE="executing"
+	# exit on errors
+	set -eE
 fi
 
 # print run type and source name
@@ -127,7 +197,25 @@ for remote_name in ${r_names}; do
 	if [[ "${remote_pro}" == "SSH" ]]; then
 		# check connection before proceeding
 		echo -n "${TAB}${fTAB}checking connection... "
+		unset_traps
 		ssh -o ConnectTimeout=1 -o ConnectionAttempts=1 -T ${remote_host}
+		RETVAL=$?
+		set_traps
+		if [[ $RETVAL == 0 ]]; then
+			echo -e "${fTAB}${GOOD}OK${NORMAL} ${gray}RETVAL=$RETVAL${NORMAL}"
+		else
+			if [[ $RETVAL == 1 ]]; then
+				echo -e "${fTAB}${yellow}FAIL${NORMAL} ${gray}RETVAL=$RETVAL${NORMAL}"
+				if [[ $remote_host =~ "github.com" ]]; then
+					decho "host is github"
+					# Github will return 1 if everything is working
+				else
+					decho "host is not github"
+				fi
+			else
+				echo -e "${fTAB}${BAD}FAIL${NORMAL} ${gray}RETVAL=$RETVAL${NORMAL}"
+			fi
+		fi
 	fi
 	if [ "${n_remotes}" -gt 1 ]; then
 		TAB=${TAB%$fTAB}
@@ -139,10 +227,10 @@ unset remote_host
 
 # parse remote tracking branch and local branch
 cbar "${BOLD}parse current settings...${NORMAL}"
-if [ -z "$(git branch -vv | grep \* | grep "\[")" ]; then
+remote_tracking_branch=$(git rev-parse --abbrev-ref @{upstream})
+if [ -z ${remote_tracking_branch+default} ]; then
     echo "${TAB}no remote tracking branch set for current branch"
 else
-    remote_tracking_branch=$(git rev-parse --abbrev-ref master@{upstream})
     echo -e "${TAB}remote tracking branch: ${blue}${remote_tracking_branch}${NORMAL}"
     upstream_repo=${remote_tracking_branch%%/*}
     echo "${TAB}${fTAB}remote name: ....... $upstream_repo"
@@ -493,7 +581,7 @@ if [ $N_temp -gt 0 ]; then
 	echo "${TAB}after rebase:"
 	N_temp=$(git rev-list ${local_branch}..${branch_temp} | wc -l)
 	echo -e "${TAB}${fTAB}${yellow}branch '${branch_temp}' is ${N_temp} commits ahead of '${local_branch}'${NORMAL}"
-		
+	
 	# merge
 	cbar "${BOLD}merging local changes...${NORMAL}"
     git checkout ${local_branch}
