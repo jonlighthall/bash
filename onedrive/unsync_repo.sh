968a7e7862ff6c8c8a09ed6dd02800d7b7dba303
Jon Lighthall <jon.lighthall@gmail.com>
2024-02-23 18:03:50 -0600

    use undel_repo for unsync

--- onedrive/unsync_repo.sh
+++ onedrive/unsync_repo.sh
@@ -6,14 +6,7 @@ echo $BASH_SOURCE
 
 unfix_bad_extensions ./
 
-# get list of deleted files
-list=$(git ls-files -d)
+undel_repo ./
 
-# checkout deleted files
-for fname in $list; do
-    echo $fname
-    git checkout $fname
-done
-wait
 # print time at exit
 echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
