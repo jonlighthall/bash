46c3865433defaf02e954203c92810f5f4666bff
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-04 14:15:06 -0600

    add make links

--- make_links.sh
+++ make_links.sh
@@ -3,14 +3,19 @@
 # get starting time in nanoseconds
 start_time=$(date +%s%N)
 
-# set tab
-:${TAB:=''}
-
 # load formatting
 fpretty="${HOME}/utils/bash/.bashrc_pretty"
 if [ -e "$fpretty" ]; then
     source "$fpretty"
     set_traps
+    # set tab
+    rtab
+fi
+
+# load formatting
+flink="${HOME}/utils/bash/.bash_links"
+if [ -e "$flink" ]; then
+    source "$flink"
 fi
 
 # determine if script is being sourced or executed
@@ -90,82 +95,7 @@ for my_link in \
     fi
     link=${link_dir}/${my_link}
 
-    # check if target exists
-    echo -n "target file ${target}... "
-    if [ -e "${target}" ]; then
-        echo "exists "
-
-        # next, check file permissions
-        if true; then
-            echo -n "${TAB}${target##*/} requires specific permissions: "
-            permOK=500
-            echo "${permOK}"
-            TAB+=${fTAB:='   '}
-            echo -n "${TAB}checking permissions... "
-            perm=$(stat -c "%a" "${target}")
-            echo ${perm}
-            # the target files will have the required permissions added to the existing permissions
-            if [[ ${perm} -le ${permOK} ]] || [[ ! (-f "${target}" && -x "${target}") ]]; then
-                echo -en "${TAB}${GRH}adding permissions${NORMAL} to ${permOK}... "
-                chmod +${permOK} "${target}" || chmod u+rx "${target}"
-                RETVAL=$?
-                if [ $RETVAL -eq 0 ]; then
-                    echo -e "${GOOD}OK${NORMAL} ${gray}RETVAL=$RETVAL${NORMAL}"
-                else
-                    echo -e "${BAD}FAIL${NORMAL} ${gray}RETVAL=$RETVAL${NORMAL}"
-                fi
-            else
-                echo -e "${TAB}permissions ${GOOD}OK${NORMAL}"
-            fi
-            TAB=${TAB%$fTAB}
-        fi
-
-        # begin linking...
-        echo -n "${TAB}link ${link}... "
-        TAB+=${fTAB:='   '}
-        # first, check for existing copy
-        if [ -L "${link}" ] || [ -f "${link}" ] || [ -d "${link}" ]; then
-            echo -n "exists and "
-            if [[ "${target}" -ef "${link}" ]]; then
-                echo "already points to ${my_link}"
-                echo -n "${TAB}"
-                ls -lhG --color=auto "${link}"
-                echo "${TAB}skipping..."
-                TAB=${TAB%$fTAB}
-                continue
-            else
-                # next, delete or backup existing copy
-                if [ $(diff -ebwB "${target}" "${link}" 2>&1 | wc -c) -eq 0 ]; then
-                    echo "has the same contents"
-                    echo -n "${TAB}deleting... "
-                    rm -v "${link}"
-                else
-                    if [ -e "${link}" ]; then
-                        echo "will be backed up..."
-                        mdate=$(date -r "${link}" +'%Y-%m-%d-t%H%M')
-                    else
-                        echo -n "is a broken link..."
-                        mdate=$(stat -c '%y' ${link} | sed 's/\(^[0-9-]*\) \([0-9:]*\)\..*$/\1-t\2/' | sed 's/://g')
-                    fi
-                    link_copy="${link}_${mdate}"
-                    mv -v "${link}" "${link_copy}" | sed "s/^/${TAB}/"
-                fi
-            fi
-        else
-            echo "does not exist"
-        fi
-        # then link
-        echo -en "${TAB}${GRH}"
-        hline 72
-        echo "${TAB}making link... "
-        ln -sv "${target}" "${link}" | sed "s/^/${TAB}/"
-        echo -ne "${TAB}"
-        hline 72
-        echo -en "${NORMAL}"
-        TAB=${TAB%$fTAB}
-    else
-        echo -e "${BAD}does not exist${NORMAL}"
-    fi
+    do_link_exe "$target" "$link"
 done
 bar 38 "--------- Done Making Links ----------"
 
