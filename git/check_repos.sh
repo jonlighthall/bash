2e0fda3a7eecf34088cd8605f856266a60d25929
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-08 14:44:00 -0600

    make variables local, update var name, use i/d tab

--- git/check_repos.sh
+++ git/check_repos.sh
@@ -19,27 +19,27 @@ function check_repos() {
         TAB=''
         : ${fTAB:='   '}
     else
-        TAB+=${TAB+${fTAB:='   '}}
+        itab
     fi
 
     # set debug level
-    declare -i DEBUG=${DEBUG:=0}
+    local -i DEBUG=${DEBUG:=0} # default value if DEBUG is unset or null
 
     # load formatting and functions
-    fpretty=${HOME}/config/.bashrc_pretty
+    local fpretty=${HOME}/config/.bashrc_pretty
     if [ -e $fpretty ]; then
         source $fpretty
         set_traps
-        decho "DEBUG = $DEBUG"
     fi
 
     # determine if script is being sourced or executed and add conditional behavior
+    local RUN_TYPE
     if (return 0 2>/dev/null); then
         RUN_TYPE="sourcing"
-        set +e
+        #set +e
     else
         RUN_TYPE="executing"
-        set -e
+        #set -e
     fi
 
     # show good hosts
@@ -50,18 +50,18 @@ function check_repos() {
     else
         host_OK=$(echo "${host_OK}" | sort -n)
         decho
-        decho -e "${GOOD}${host_OK}${NORMAL}" | sed "s/^/${fTAB}/"
+        decho -e "${GOOD}${host_OK}${NORMAL}" | sed "$ ! s/^/${fTAB}/"
     fi
 
     # show bad hosts
-    decho -n "existing bad hosts: "
+    decho -n "existing  bad hosts: "
     if [ -z "${host_bad:+dummy}" ]; then
         decho "none"
         export host_bad=''
     else
         host_bad=$(echo "${host_bad}" | sort -n)
         decho
-        decho -e "${BAD}${host_bad}${NORMAL}" | sed "s/^/${fTAB}/"
+        decho -e "${BAD}${host_bad}${NORMAL}" | sed "$ ! s/^/${fTAB}/"
     fi
 
     # check if Git is defined
@@ -71,10 +71,10 @@ function check_repos() {
             echo -e "${GOOD}OK${NORMAL} Git is defined"
             # get Git version
             git --version | sed "s/^/${fTAB}/"
-            git_ver=$(git --version | awk '{print $3}')
-            git_ver_maj=$(echo $git_ver | awk -F. '{print $1}')
-            git_ver_min=$(echo $git_ver | awk -F. '{print $2}')
-            git_ver_pat=$(echo $git_ver | awk -F. '{print $3}')
+            export git_ver=$(git --version | awk '{print $3}')
+            export git_ver_maj=$(echo $git_ver | awk -F. '{print $1}')
+            export git_ver_min=$(echo $git_ver | awk -F. '{print $2}')
+            export git_ver_pat=$(echo $git_ver | awk -F. '{print $3}')
             export check_git=false
         else
             echo -e "${BAD}FAIL${NORMAL} Git not defined"
@@ -87,18 +87,19 @@ function check_repos() {
     fi
 
     # get number of remotes
-    n_remotes=$(git remote | wc -l)
-    r_names=$(git remote)
+    local -i n_remotes=$(git remote | wc -l)
+    local r_names=$(git remote)
     echo "remotes found: ${n_remotes}"
-    declare -i i=0
+    local -i i=0
     for remote_name in ${r_names}; do
         if [ "${n_remotes}" -gt 1 ]; then
             ((++i))
             echo -n "${TAB}${fTAB}$i) "
-            TAB+=${fTAB:='   '}
+            itab
         fi
         # get URL
         echo "$remote_name"
+        local remote_url
         if [ $git_ver_maj -lt 2 ]; then
             remote_url=$(git remote -v | grep ${remote_name} | awk '{print $2}' | uniq)
         else
@@ -106,6 +107,8 @@ function check_repos() {
         fi
 
         # parse protocol
+        local remote_pro
+        local remote_host
         remote_pro=$(echo ${remote_url} | sed 's/\(^[^:@]*\)[:@].*$/\1/')
         if [[ "${remote_pro}" == "git" ]]; then
             remote_pro="SSH"
@@ -128,19 +131,19 @@ function check_repos() {
         fi
         if [[ "${remote_pro}" == "SSH" ]]; then
             # default to checking host
-            do_check=true
+            local do_connect=true
             host_stat=$(echo -e "${yellow}CHECK${NORMAL}")
-            decho "do_check = $do_check"
-
+            decho "do_connect = $do_connect"
+            itab
             # check remote host name against list of checked hosts
             if [ ! -z ${host_OK:+dummy} ] || [ ! -z ${host_bad:+dummy} ]; then
-                decho "checking $remote_host against list of checked hosts..."
+                decho "${TAB}checking $remote_host against list of checked hosts..."
                 if [ ! -z ${host_OK:+dummy} ]; then
                     for good_host in ${host_OK}; do
                         if [[ $remote_host =~ $good_host ]]; then
                             decho "${TAB}${remote_host} matches $good_host"
                             host_stat=$(echo -e "${GOOD}OK${NORMAL}")
-                            do_check=false
+                            do_connect=false
                             break
                         fi
                     done
@@ -151,28 +154,29 @@ function check_repos() {
                         if [[ "$remote_host" == "$bad_host" ]]; then
                             decho "${TAB}${remote_host} matches $bad_host"
                             host_stat=$(echo -e "${BAD}FAIL${NORMAL}")
-                            do_check=false
+                            do_connect=false
                             break
                         fi
                     done
                 fi
             fi
         else
-            do_check=false
-            host_stat=$(echo -e "${gray}CHECK{NORMAL}")
+            do_connect=false
+            host_stat=$(echo -e "${gray}CHECK${NORMAL}")
         fi # SSH
 
         (
-            echo "${TAB}${fTAB}url+ ${remote_url}"
-            echo -e "${TAB}${fTAB}host+ ${remote_host} ${host_stat}"
-            echo -e "${TAB}${fTAB}proto+ ${remote_pro}"
+            echo    "${TAB}url+ ${remote_url}"
+            echo -e "${TAB}host+ ${remote_host} ${host_stat}"
+            echo -e "${TAB}proto+ ${remote_pro}"
         ) | column -t -s+ -o : -R 1
-        decho "do_check = $do_check"
-
+        decho "do_connect = $do_connect"
+        dtab
         # check connection before proceeding
-        if [ ${do_check} = 'true' ]; then
+        unset_traps
+        if [ ${do_connect} = 'true' ]; then
             echo -n "${TAB}${fTAB}checking connection... "
-            unset_traps
+
             ssh_cmd_base="ssh -o ConnectTimeout=3 -o ConnectionAttempts=1 -T ${remote_host}"
             if [[ "${remote_host}" == *"navy.mil" ]]; then
                 $ssh_cmd_base -o LogLevel=error 2> >(sed $'s,.*,\e[31m&\e[m,' >&2) 1> >(sed $'s,.*,\e[32m&\e[m,' >&1)
@@ -220,10 +224,13 @@ function check_repos() {
                     host_bad+=${remote_host}
                 fi # retval 1
             fi # retval 0
+        else
+            decho "skipping connection check..."
+            continue
         fi # do check
 
         if [ "${n_remotes}" -gt 1 ]; then
-            TAB=${TAB%$fTAB}
+            dtab
         fi
     done
     unset remote_url
