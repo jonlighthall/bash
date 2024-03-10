6bf6dbc3ff32162d30cabb835a501b096c31e745
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-10 15:53:47 -0500

    fix set path: add dirname, warning, readlink, fix timestamp

--- set_path.sh
+++ set_path.sh
@@ -3,31 +3,42 @@
 # PATH. Add the directories one and two levels up from the present working directory to the start
 # of the PATH environmental variable.
 #
+# Jun 2022 JCL
+
 # NB: remember to 'source' this script, don't just execute it
-#
+if ! (return 0 2>/dev/null); then
+    # exit on errors
+    set -e
+    # print note
+    echo -e "\x1b[31mNOTICE: this script must be soured to save changes to path!\x1b[m"
+fi
+
 # Path additions
-for ADDPATH in \
-	${PWD%/*}/ \
-	${PWD%/*/*}/; do
-	echo -n ${ADDPATH}
-	# check if path exists
-	if [ -d "${ADDPATH}" ]; then
-		echo " found"
-		ABSPATH=$(cd $ADDPATH; \pwd)
-		echo -n ${ABSPATH}
-	else
-		echo " not found"
-		continue
-	fi
-	# prepend path to PATH
-	if [[ "$PATH" != *"${ADDPATH}:"* ]]; then
-		export PATH=$ADDPATH:$PATH
-		echo " added to PATH"
-	else
-		echo " already in PATH"
-	fi
+path=$(dirname $PWD)
+path+=( $(dirname $path) )
+
+for ADDPATH in ${path[@]}; do
+	  echo -en "   \x1b[33m${ADDPATH}\x1b[m... "
+	  # check if path exists
+	  if [ -d "${ADDPATH}" ]; then
+        if ! (return 0 2>/dev/null); then
+		        echo "found"
+            continue
+        fi
+		    ABSPATH=$(readlink -f $ADDPATH)
+	  else
+		    echo "not found"
+		    continue
+	  fi
+	  # prepend path to PATH
+	  if [[ "$PATH" != *"${ADDPATH}:"* ]]; then
+		    export PATH=$ABSPATH:$PATH
+		    echo "added to PATH"
+	  else
+		    echo "already in PATH"
+	  fi
 done
 echo "done"
 
 # print time at exit
-echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
+echo -e "\n${BASH_SOURCE##*/} "$(sec2elap $SECONDS)"on $(date +"%a %b %-d %-l:%M %p %Z")"
