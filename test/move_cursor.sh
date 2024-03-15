899ab3998b2ed1b459f6aef59809c491cd75d80c
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-15 11:54:04 -0500

    add dummy bracket

--- test/move_cursor.sh
+++ test/move_cursor.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -u
 function cursor_pos() {
-	# get the cursor position
-	echo -en "\E[6n"
-	read -sdR CURPOS
-	# parse the cursor position
-	CURPOS=${CURPOS#*[}
-	x_pos=${CURPOS#*;}
-	y_pos=${CURPOS%;*}
-	# print the cursor position
-	echo -e "\nposition is x=$x_pos y=$y_pos"
+	  # get the cursor position
+	  echo -en "\E[6n"
+	  read -sdR CURPOS
+	  # parse the cursor position
+	  CURPOS=${CURPOS#*[}
+          #}# dummy bracket for emacs indenting
+	  x_pos=${CURPOS#*;}
+	  y_pos=${CURPOS%;*}
+	  # print the cursor position
+	  echo -e "\nposition is x=$x_pos y=$y_pos"
 }
 
 clear -x
@@ -22,6 +23,7 @@ set +x
 echo -en "\E[6n"
 read -sdR CURPOS
 CURPOS=${CURPOS#*[}
+      #}# dummy bracket for emacs indenting
 cx1=${CURPOS#*;}
 cy1=${CURPOS%;*}
 echo -en "\E[${y_pos};${x_pos}H \E[1;31mthere $x_pos $y_pos\E[0m"
