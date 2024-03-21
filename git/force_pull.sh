43c68ad62e66c604fc6ce7f8c710fa8a1a3aa45b
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-21 17:40:37 -0500

    update header

--- git/force_pull.sh
+++ git/force_pull.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -u
+# -----------------------------------------------------------------------------------------------
 #
-# git/force_pull.sh - this script was developed to synchronize the local repository with the
-# remote repository after a force push; hence the name. It assumes that---in the case of a force
-# push---that the two repsoitories have common commit times, if not common hashes; this would be
-# the case the history has been rewritten to update author names, for example. It is also useful
-# for synchonizing diverged repsoitories without explicitly merging.
+# git/force_pull.sh
 #
-# METHOD -
+# PURPOSE: This script was developed to synchronize the local repository with the remote
+#   repository after a force push; hence the name. It assumes that---in the case of a force
+#   push---that the two repsoitories have common commit times, if not common hashes; this would
+#   be the case the history has been rewritten to update author names, for example. It is also
+#   useful for synchonizing diverged repsoitories without explicitly merging.
+#
+# METHOD:
 #   STASH  save uncommited local changes
 #   BRANCH copy local commits to temporary branch
 #   RESET  reset local branch to common remote commit
@@ -17,10 +20,12 @@
 #   STASH  restore uncommited changes
 #   RESET  unstage uncommited changes
 #
-# USAGE - the remote name and branch can be optionally specified by the first and second
-# arguments, respectively. The default remote branch is the current tracking branch.
+# USAGE: The remote name and branch can be optionally specified by the first and second
+#   arguments, respectively. The default remote branch is the current tracking branch.
 #
 # Apr 2023 JCL
+#
+# -----------------------------------------------------------------------------------------------
 
 # get starting time in nanoseconds
 declare -i start_time=$(date +%s%N)
