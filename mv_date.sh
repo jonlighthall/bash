367265af5bb8881408705bfd8893078f51fb2e2b
Jon Lighthall <jon.lighthall@gmail.com>
2024-03-23 15:46:49 -0500

    add formatting

--- mv_date.sh
+++ mv_date.sh
@@ -12,26 +12,29 @@
 # -----------------------------------------------------------------------------------------------
 
 function get_mod_date() {
-    trap 'print_return $?' RETURN
-    echo -e "${INVERT}function: ${FUNCNAME}${RESET}"
+    trap 'print_return $?;dtab' RETURN
+    itab
+    echo -e "${TAB}${INVERT}function: ${FUNCNAME}${RESET}"
     if [ $# -lt 2 ]; then
-	      echo "Please provide an input file"
+	      echo "${TAB}Please provide an input file"
 	      return 1
     fi
-    echo "number of arguments = $#"
+    echo "${TAB}number of arguments = $#"
 
+    itab
     local -r in_file="$(readlink -f "$1")"
-    echo "argument 1: $1"
+    echo "${TAB}argument 1: $1"
 
     local -n output=$2
-    echo "argument 2: $2"
+    echo "${TAB}argument 2: $2"
+    dtab
     
-    TAB+=${fTAB:='   '}
     echo -n "${TAB}input path: ${in_file}... "
+    # check if input exists
     if [ -L "${in_file}" ] || [ -f "${in_file}" ] || [ -d "${in_file}" ]; then
-	      echo "exits"
-
+	      echo -e "${GOOD}exists${RESET}"
 	      # parse input
+
         # directory name
         in_dir="$(dirname "${in_file}")"
         # base name
@@ -44,19 +47,20 @@ function get_mod_date() {
 	      else
 		        ext=""
 	      fi
-        # redefine if hidden file
+        # redefine "extension" if a hidden file
         if [ -z "${in_base}" ] && [[ "${in_fname::1}" == "." ]]; then
             in_base="${ext}"
             ext=''
         fi
-
+        
+        # print summary
         (
-            echo "input dir: $in_dir"
-	          echo "input file: $in_fname"
-	          echo "base name: $in_base"
-	          echo -n "ext name: ${ext}"
+            decho "input dir: $in_dir"
+	          decho "input file: $in_fname"
+	          decho "base name: $in_base"
+	          decho -n "ext name: ${ext}"
 	          if [ ${#ext} -eq 0 ]; then
-		            echo "EMPTY"
+		            decho -e "${GRAY}EMPTY${RESET}"
 	          else
 		            echo
 	          fi
@@ -67,50 +71,58 @@ function get_mod_date() {
 	      output="${in_dir}/${out_base}${ext}"
 
 	      # check if input and output are the same file
-	      echo -e "output file ${output} is ..."
+	      echo -e "${TAB}output file ${output} is..."
+        itab
 	      while [ "${in_file}" -ef "${output}" ]; do
-		        echo "${TAB}the same file as input file ${in_file}"
+		        echo -e "${TAB}${YELLOW}the same file${RESET} as input file ${in_file}"
 		        echo -n "${TAB}renaming output... "
 		        # NB: don't rename any existing files; change the ouput file name to something unique
 		        output=${in_dir}/${out_base}_$(date -r "${output}" +'%Y-%m-%d-t%H%M%S')${ext}
 		        echo ${output}
 	      done
-	      echo "${TAB}uniquely named"
+	      ddecho "${TAB}named differently than input"
+        dtab
 
 	      # check if output exists
-	      echo "output file ${output}... "
+	      echo "${TAB}output file ${output}... "
+        itab
 	      if [ -f "${output}" ]; then
-		        echo "${TAB}exists"
-		        echo -n "${TAB}waiting for new time stamp... "
+		        echo -e "${TAB}${BAD}exists${RESET}"
+		        ddecho -n "${TAB}waiting for new time stamp... "
 		        while [ -f "${output}" ]; do
 			          # NB: don't rename any existing files; change the ouput file name to something
 			          # unique
 			          output=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
 		        done
-		        echo "done"
-		        echo "${TAB}unique file name found"
+		        ddecho "done"
+		        ddecho "${TAB}unique file name found"
 		        output=${in_dir}/${out_base}_$(date +'%Y-%m-%d-t%H%M%S')${ext}
-		        echo "output file ${output}"
+		        echo "${TAB}output file ${output}"
 	      else
-		        echo "${TAB}does not exist (uniquely named)"
+		        echo -e "${TAB}${GOOD}does not exist${RESET} (uniquely named)"
 	      fi
+        dtab
     else
 	      echo "is not valid"
-	      test_file $1 | sed "s/^/${TAB}/"
+        itab
+	      test_file "$1" | sed "s/^/${TAB}/"
 	      if [ -L "$1" ]; then
 		        in_file=$1
 		        echo "${in_file} is a broken link!"
 		        mdate=$(stat -c '%y' "${in_file}" | sed 's/\(^[0-9-]*\) \([0-9:]*\)\..*$/\1-t\2/' | sed 's/://g')
 		        output=$1_${mdate}
 	      else		
-		        echo "${TAB}exiting..."
+            dtab 
+		        echo -e "${TAB}${BAD}exiting...${RESET}"
+            dtab 
 		        exit 1
 	      fi
+        dtab
     fi
 }
 
 # set debug level
-declare -i DEBUG=0
+declare -i DEBUG=1
 
 # load formatting and functions
 fpretty=${HOME}/config/.bashrc_pretty
@@ -124,15 +136,20 @@ if [ $# -eq 0 ]; then
 	  echo "Please provide an input file"
 	  exit 1
 fi
-echo "number of arguments = $#"
+decho "${TAB}number of arguments = $#"
 
 # set file names
 in_file=$(readlink -f $1)
-echo "argument 1: $1"
-
+itab
+decho "${TAB}argument 1: $1"
+dtab
+echo "${TAB}getting modifcation date..."
 declare out_file
-
 get_mod_date "${in_file}" out_file
 
 # now move file
+echo "${TAB}moving file..."
+itab
+echo -n "${TAB}"
+dtab
 mv -nv "${in_file}" "${out_file}"
