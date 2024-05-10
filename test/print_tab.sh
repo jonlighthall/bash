79fa04288b7b49ebf6cc02bbb399edf32e22b94b
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-10 15:00:04 -0500

    update var names in print tab

--- test/print_tab.sh
+++ test/print_tab.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -u
 
 # define colors
-declare -r yellow='\E[0;33m' # yellow
-declare -r   GOOD='\E[0;32m' # green
-declare -r NORMAL='\E[0m'    # reset
-declare -r  space='\E[30;106m' # highlight white space
+declare -r YELLOW=${YELLOW:-'\E[0;33m'} # yellow
+declare -r   GOOD=${GOOD:-'\E[0;32m'}   # green
+declare -r NORMAL=${NORMAL:-'\E[0m'}    # reset
+declare -r  SPACE='\E[30;106m' # highlight white space
 
 # set lcoal tab
 declare -r thisTAB='   '
@@ -21,7 +21,7 @@ do
 
 	#print variable value
 	val=${!var}
-	echo -en "${GOOD}set${RESET} to ${space}${val}${RESET}"
+	echo -en "${GOOD}set${RESET} to ${SPACE}${val}${RESET}"
 
 	# print variable length
 	i=${#val}
