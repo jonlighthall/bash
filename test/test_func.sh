84d730358ec56591f08f54c568ba1e15de80a322
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-13 10:52:57 -0500

    add formatting

--- test/test_func.sh
+++ test/test_func.sh
@@ -9,19 +9,21 @@ if [ -e $fpretty ]; then
 	  source $fpretty
 fi
 
-for func in hello bar hline potato
+for func in $@ hello bar hline potato
 do
     echo "${TAB}testing if $func is a function"
     itab
     if [ "$(type -t $func)" != function ]; then
 	      echo "${TAB}$func not a function"
-        echo "${TAB}redefining $func as a no-op function"        
+        echo "${TAB}redefining $func as a no-op function..."        
 	      eval "$func() {
 	    :
 	}"
     else
-	      echo "${TAB}$func is a function:"
-	      $func
+	      echo "${TAB}$func is a function"
     fi
-    dtab
+    echo "${TAB}output:"
+    itab
+    $func | sed "s/^/${TAB}/"
+    dtab 2
 done
