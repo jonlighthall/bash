ff790ae52053f0a7ff60361f9f20053a7d4af572
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-19 00:52:31 -0500

    fix force pull print

--- git/update_repos.sh
+++ git/update_repos.sh
@@ -583,12 +583,13 @@ for repo in $list; do
                 fi
                 # force pull
                 if [[ $RETVAL == 128 ]]; then
-                    cbar "{GRH}force pull...${RESET}"
-                    echo -n "${TAB}leading remote commits: ${N_remote}"
-                    echo -n "${TAB}trailing local commits: "
+                    cbar -e "{GRH}force pull...${RESET}"
+                    echo "${TAB}leading remote commits: ${N_remote}"
+                    echo "${TAB}trailing local commits: "
                     N_local=$(git rev-list ${remote_tracking_branch}..HEAD | wc -l)
                     echo "${N_local}"
                     if [ $N_local -gt 0 ] && [ $N_remote -gt 0 ]; then
+                        local_branch=$(git branch | grep \* | sed 's/^\* //')
                         echo -e "${fTAB}${YELLOW}local '${local_branch}' and remote '${pull_branch}' have diverged${RESET}"
                     fi
                     echo -e "${TAB}source directory = $src_dir_logi"
