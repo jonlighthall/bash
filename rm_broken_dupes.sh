6e6b912331ded3350a266acb6a1f87c60969f59d
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-20 15:10:52 -0500

    add remove broken links

---
+++ rm_broken_dupes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -u
+
+# load bash utilities
+fpretty="${HOME}/config/.bashrc_pretty"
+if [ -e "$fpretty" ]; then
+    source "$fpretty"
+fi
+
+# set shell options
+if [[ "$-" == *e* ]]; then
+    # exit on errors must be turned off; otherwise shell will exit no remote branch found
+    old_opts=$(echo "$-")
+    set +e
+fi
+unset_traps
+
+# check for input
+if [ $# -eq 0 ]; then
+	  echo "Please provide an input file"
+else
+	  # check arguments
+	  for arg in "$@"; do
+		    if [ -L "${arg}" ]; then
+			      if [ ! -e "${arg}" ]; then
+                echo -en "${YELLOW}$arg${RESET} "
+                echo -n "is a "
+				        echo -e -n "${BROKEN}broken${RESET}"
+			          echo -e " ${UL}link${RESET}"
+                ls -l --color ${arg} | sed 's,^.*\(\./\),\1,'
+
+                og_fname=$(echo ${arg} | sed 's/_[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}-t[0-9]\{6\}//')
+                echo -n "${TAB}${og_fname}..."
+
+                if [ -e "${og_fname}" ]; then
+                    echo "exists"
+                    itab
+                    echo "${TAB}deleting $arg..."
+                    echo -n ${TAB}
+                    rm -v ${arg}
+                    dtab
+                else
+                    echo "not found"
+                fi
+
+                
+			      fi
+		    fi
+	  done
+fi
+
+reset_shell ${old_opts-''}
+reset_traps
