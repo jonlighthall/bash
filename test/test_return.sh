e43bb9beddc517b9c537dd0ad06936cee223ab93
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-24 10:07:22 -0500

    add enable exit on fail to test return and update; fix fname in update

--- test/test_return.sh
+++ test/test_return.sh
@@ -6,68 +6,34 @@ DEBUG=0
 fpretty="${HOME}/config/.bashrc_pretty"
 if [ -e "${fpretty}" ]; then
     source "${fpretty}"
-    print_debug
- #   set_traps
+    clear_traps
 fi
 
 # determine if script is being sourced or executed and add conditional behavior
 if (return 0 2>/dev/null); then
     RUN_TYPE="sourcing"
-
+    #enable_exit_on_fail
 else
     RUN_TYPE="executing"
-    # exit on errors
-
+    set_traps
 fi
-
-get_source
-echo -e "${INVERT}$src_base${NORMAL} Run type = ${RUN_TYPE}"
-
-lecho
 print_source
+
+# exit on errors
 set -e
-
-function cleanup () {
-    echo -e "${INVERT}${FUNCNAME}${NORMAL}"
-    echo "${BASH_SOURCE[0]##*/}"    
-}
-
-function do_return() {
-    echo -e "${INVERT}${FUNCNAME}${NORMAL}"
-    echo "${BASH_SOURCE[0]##*/}"
-    print_stack
-    return
-
-}
-
-
-set -e
-set_traps
-
 
 echo "function:"
 itab
+# this line will cause error if not sourced
 fello
+
 dtab
 echo "alias:"
 itab
 echo -n "${TAB}"
 hello
 dtab
-
-lecho
-echo "here"
-#DEBUG=0
-#clear_traps
-trap do_return EXIT
-
-
-#trap 'echo "${FUNCNAME} return"; trap -- RETURN; trap -- EXIT; trap -- ERR; set +eE' RETURN
-#trap 'echo "${FUNCNAME} exit -> return"; trap -- RETURN; trap -- EXIT; trap -- ERR' EXIT
-set +ET
+lecho -e "${GOOD}here${RESET}"
+huh
 exit_on_fail
-
-
-#return && exit_on_fail
-exit_on_fail
-echo "there"
+lecho -e "${BAD}there${RESET}"
