5029c4a28cb3d0b7c006c17ba018bf4503ad2ff0
Jon Lighthall <jon.lighthall@gmail.com>
2024-05-31 11:55:59 -0500

    add handling for missing file names and names with spaces

--- onedrive/rm_onedrive_dupes.sh
+++ onedrive/rm_onedrive_dupes.sh
@@ -1,20 +1,36 @@
+# remove duplicate images
 
 dname=diff.jpg
 
 for fname in *-JCL-*; do
     echo $fname
+
+    if [ -e "$fname" ]; then
+        echo "file found"
+    else
+        echo "no files found"
+        echo "exiting..."
+        exit 1
+    fi
+
     fname2=${fname/-JCL-Spectre-i7-Vega/}
     echo $fname2
 
-    compare -compose src ${fname} ${fname2} ${dname}
+    if [ -e "$fname2" ]; then
+
+    compare -compose src "${fname}" "${fname2}" ${dname}
 
     convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:
 
     if [ $(convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:) -eq 1 ]; then        
         echo "delte"
-        rm -v ${fname} ${dname}
+        rm -v "${fname}" ${dname}
     else
         echo "keep"
+    fi
+
+    else
+        echo "not found"
     fi
     
 done
