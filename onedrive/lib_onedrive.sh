063c212629eee0cafc1409e7f73676464ca1713c
Jon Lighthall <jon.lighthall@gmail.com>
2024-06-13 11:02:15 -0500

    add onedrive lib

---
+++ onedrive/lib_onedrive.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+# define replacement seperator
+sep=_._
+
+declare -i count_found=0
+declare -i count_rm=0
+declare -i count_mv=0
+declare -i count_mv_fail=0
+declare -i count_skip=0
+
+declare -a bad_list
+bad_list=( "bat" "bin" "cmd" "csh" "exe" "gz" "js" "ksh" "osx" "out" "prf" "ps" "ps1" )
+
+function check_arg() {
+    # check argument
+    if [ $# -eq 0 ]; then
+	      echo "Please provide a target directory"
+	      exit 1
+    fi
+
+    # find input directory
+    if [[ "$1" == "." ]]; then
+        in_dir=$(readlink -f "$PWD/$1")
+    else
+        in_dir=$(readlink -f $1)
+    fi
+    echo -n "target directory $in_dir..."
+    if [ ! -d "${in_dir}" ]; then
+		    echo "not found"
+		    exit 1
+    fi
+    echo "found "
+    cd "${in_dir}"
+}
+
+function print_stat() {
+    echo
+    echo -e "${UL}Files found: $count_found${NORMAL}"
+    echo "Files deleted: $count_rm"
+    echo "Files skipped: $count_skip"
+    echo "Files renamed: $count_mv"
+    echo "Files not renamed: $count_mv_fail"
+    echo
+}
+
+function fix_bad_ext() {
+    # look for bad extensions
+    echo -n "${TAB}checking for files with bad extensions... "
+    itab
+    for bad in ${bad_list[@]}; do
+        # find bad files
+        name_list=$(find ./ -name "*.${bad}")
+
+        # if list is empty, continue
+        if [ -z "${name_list}" ]; then
+            continue
+        fi
+
+        # print current extension
+        start_new_line
+        echo -n "${TAB}.${bad}... "
+        itab
+
+        # print number of matches
+        local -i n_files=$(echo "$name_list" | wc -l)
+        echo "$n_files files found"
+
+        # loop over file names
+        for fname in ${name_list[@]}; do
+            ((++count_found))
+            echo -n "${TAB}${count_found}) $fname... "
+
+            # check if the file is tracked
+            git ls-files --error-unmatch "$fname" &>/dev/null
+            RETVAL=$?
+            if [ $RETVAL -eq 0 ]; then
+                echo -n "tracked: "
+
+                # check if the file is modified
+                if [ -z "$(git diff $fname)" ]; then
+                    echo -n "unmodified: "
+                    rm -v "$fname"
+                    ((++count_rm))
+                else
+                    echo -eb "${CYAN}modified: ${RESET}"
+                    mv -nv "$fname" "$(echo "$fname" | sed "s/\.$bad/$sep$bad/")"
+                    if [ -f "$fname" ];then
+                        echo "rename $fname FAILED"
+                        ((++count_mv_fail))
+                    else
+                        ((++count_mv))
+                    fi
+                fi
+            else
+                echo -en "${MAGENTA}untracked: ${RESET}"
+
+                git check-ignore "${fname}" &>/dev/null
+                RETVAL=$?
+                if [ $RETVAL -eq 0 ]; then
+                    echo -n "ignored: "
+
+                    rm -v "$fname"
+                    ((++count_rm))
+                else
+                    echo -n "not ignored: "
+
+                    fname_out=$(echo "$fname" | sed "s/\.$bad/$sep$bad/")
+                    echo
+                    itab
+                    decho "${TAB}fname: $fname"
+                    decho "${TAB}fname out: $fname_out"
+                    decho "${TAB}mv -nv $fname ${fname_out}"
+                    echo -n "${TAB}"
+                    dtab
+                    mv -nv "$fname" "${fname_out}"
+                    if [ -f "$fname" ];then
+                        echo "rename $fname FAILED"
+                        ((++count_mv_fail))
+                    else
+                        ((++count_mv))
+                    fi
+                fi
+            fi
+
+        done
+        dtab
+    done
+    dtab
+    echo "done"
+
+}
+
+function fix_bin() {
+    # first, remove tracked files from the repository
+    echo -n "${TAB}removing tracked binary (and empty) files from the repository... "
+    itab
+    for fname in $(find ./ -not -path "*$GITDIR/*" -not -path "*/*git/*" -type f ); do
+
+        # Check if the file is binary
+        if perl -e 'exit -B $ARGV[0]' "$fname"; then
+            :  #echo "File is text."; file $file
+        else
+            start_new_line
+            echo -en "${TAB}${fname##*./}... "
+            if [ ! -s $fname ]; then
+                echo -en "${YELLOW}empty: ${RESET}"
+                rm -v "$fname"
+                ((++count_rm))
+                continue
+            else
+                echo -ne "${BAD}binary: ${RESET}"
+            fi
+
+            # check if the file is tracked
+            git ls-files --error-unmatch $fname &>/dev/null
+            RETVAL=$?
+            if [ $RETVAL -eq 0 ]; then
+                echo -n "tracked: "
+                # check if the file is modified
+                if [ -z "$(git diff $fname)" ]; then
+                    echo -n "unmodified: "
+                    rm -v "$fname"
+                    ((++count_rm))
+                else
+                    echo -e "${CYAN}modified${RESET}"
+                    ((++count_skip))
+                fi
+            else
+                echo -e "${MAGENTA}untracked${RESET}"
+                ((++count_skip))
+            fi
+        fi
+    done
+    echo "done"
+    dtab
+
+}
--- onedrive/rm_tracked_bad_extensions.sh
+++ onedrive/rm_tracked_bad_extensions.sh
@@ -12,32 +12,18 @@ declare -i start_time=$(date +%s%N)
 fpretty=${HOME}/config/.bashrc_pretty
 if [ -e $fpretty ]; then
     source $fpretty
+    print_source
 fi
 
-# define replacement seperator
-sep=_._
+# load onedrive utilities
+flib=${HOME}/utils/bash/onedrive/lib_onedrive.sh
+if [ -e $flib ]; then
+    source $flib
+fi
 
 trap 'echo "${TAB}exiting ${BASH_SOURCE[0]##*/}..."' EXIT
 
-# check argument
-if [ $# -eq 0 ]; then
-	  echo "Please provide a target directory"
-	  exit 1
-fi
-
-# find input directory
-if [[ "$1" == "." ]]; then
-    in_dir=$(readlink -f "$PWD/$1")
-else
-    in_dir=$(readlink -f $1)
-fi
-echo -n "target directory $in_dir..."
-if [ ! -d "${in_dir}" ]; then
-		echo "not found"
-		exit 1
-fi
-echo "found "
-cd "${in_dir}"
+check_arg "$@"
 
 itab
 # check if PWD is a git repository
@@ -63,7 +49,7 @@ if [[ ${PWD} -ef ${repo_dir} ]]; then
 else
     echo "$PWD is part of a Git repository"
     echo "moving to top level directory..."
-    cd -L $repo_dir
+    cd -L "$repo_dir"
     echo "$PWD"
 fi
 
@@ -79,159 +65,17 @@ fi
 echo "${TAB}executing git fetch..."
 do_cmd git fetch --verbose --all --prune
 echo "${TAB}done"
-
-declare -i count_found=0
-declare -i count_rm=0
-declare -i count_mv=0
-declare -i count_mv_fail=0
-declare -i count_skip=0
+do_cmd git gc
 
 set -E +e
 trap -- ERR
 
-function fix_bin() {
-    # first, remove tracked files from the repository
-    echo -n "${TAB}removing tracked binary (and empty) files from the repository... "
-    itab
-    for fname in $(find ./ -not -path "*$GITDIR/*" -not -path "*/*git/*" -type f ); do
-
-        # Check if the file is binary
-        if perl -e 'exit -B $ARGV[0]' "$fname"; then
-            :  #echo "File is text."; file $file
-        else
-            start_new_line
-            echo -en "${TAB}${fname##*./}... "
-            if [ ! -s $fname ]; then
-                echo -en "${YELLOW}empty: ${RESET}"
-                rm -v "$fname"
-                ((++count_rm))
-                continue
-            else
-                echo -ne "${BAD}binary: ${RESET}"
-            fi
-
-            # check if the file is tracked
-            git ls-files --error-unmatch $fname &>/dev/null
-            RETVAL=$?
-            if [ $RETVAL -eq 0 ]; then
-                echo -n "tracked: "
-                # check if the file is modified
-                if [ -z "$(git diff $fname)" ]; then
-                    echo -n "unmodified: "
-                    rm -v "$fname"
-                    ((++count_rm))
-                else
-                    echo -e "${CYAN}modified${RESET}"
-                    ((++count_skip))
-                fi
-            else
-                echo -e "${MAGENTA}untracked${RESET}"
-                ((++count_skip))
-            fi
-        fi
-    done
-    echo "done"
-    dtab
-
-}
-
-function fix_bad_ext() {
-    # look for bad extensions
-    echo -n "${TAB}checking for files with bad extensions... "
-    itab
-    for bad in bat bin cmd csh exe gz js ksh osx out prf ps ps1; do
-
-        name_list=$(find ./ -name "*.${bad}")
-
-        if [ -z "${name_list}" ]; then
-            continue
-        fi
-
-        start_new_line
-        echo -n "${TAB}.${bad}... "
-        itab
-
-        n_files=$(echo "$name_list" | wc -l)
-        echo "$n_files files found"
-
-        for fname in $(find ./ -name "*.${bad}"); do
-            ((++count_found))
-            echo -n "${TAB}${count_found}) $fname... "
-
-            # check if the file is tracked
-            git ls-files --error-unmatch "$fname" &>/dev/null
-            RETVAL=$?
-            if [ $RETVAL -eq 0 ]; then
-                echo -n "tracked: "
-
-                # check if the file is modified
-                if [ -z "$(git diff $fname)" ]; then
-                    echo -n "unmodified: "
-                    rm -v "$fname"
-                    ((++count_rm))
-                else
-                    echo -n "modified: "
-                    mv -nv "$fname" "$(echo "$fname" | sed "s/\.$bad/$sep$bad/")"
-                    if [ -f "$fname" ];then
-                        echo "rename $fname FAILED"
-                        ((++count_mv_fail))
-                    else
-                        ((++count_mv))
-                    fi
-                fi
-            else
-                echo -n "untracked: "
-
-                git check-ignore "${fname}" &>/dev/null
-                RETVAL=$?
-                if [ $RETVAL -eq 0 ]; then
-                    echo -n "ignored: "
-
-                    rm -v "$fname"
-                    ((++count_rm))
-                else
-                    echo -n "not ignored: "
-
-                    fname_out=$(echo "$fname" | sed "s/\.$bad/$sep$bad/")
-                    echo
-                    itab
-                    decho "${TAB}fname: $fname"
-                    decho "${TAB}fname out: $fname_out"
-                    decho "${TAB}mv -nv $fname ${fname_out}"
-                    echo -n "${TAB}"
-                    dtab
-                    mv -nv "$fname" "${fname_out}"
-                    if [ -f "$fname" ];then
-                        echo "rename $fname FAILED"
-                        ((++count_mv_fail))
-                    else
-                        ((++count_mv))
-                    fi
-                fi
-            fi
-
-        done
-        dtab
-    done
-    dtab
-    echo "done"
-
-}
-
 fix_bin
 
 if command -v wsl.exe >/dev/null; then
-	  echo -n "${TAB}WSL defined."
+	  echo -n "${TAB}WSL defined: "
     fix_bad_ext
 fi
 
-echo
-
-echo -e "${UL}Files found: $count_found${NORMAL}"
-echo "Files deleted: $count_rm"
-echo "Files skipped: $count_skip"
-echo "Files renamed: $count_mv"
-echo "Files not renamed: $count_mv_fail"
-echo
-
+print_stat
 set_exit
