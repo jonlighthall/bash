17a7ffcc1b176f2f0bb4b0b0ea54a81604f462f3
Jon Lighthall <jon.lighthall@gmail.com>
2024-06-17 11:02:25 -0500

    add list of suffixes

--- onedrive/rm_onedrive_dupes.sh
+++ onedrive/rm_onedrive_dupes.sh
@@ -2,35 +2,38 @@
 
 dname=diff.jpg
 
-for fname in *-JCL-*; do
-    echo $fname
+list=( "-DTWUSC001" "-LPWUD110" "-JCL-Spectre-i7-Vega" )
+for suff in ${list[@]}; do
+    for fname in *${suff}*; do
+        echo $fname
 
-    if [ -e "$fname" ]; then
-        echo "file found"
-    else
-        echo "no files found"
-        echo "exiting..."
-        exit 1
-    fi
+        if [ -e "$fname" ]; then
+            echo "file found"
+        else
+            echo "no files found"
+            echo "exiting..."
+            exit 1
+        fi
 
-    fname2=${fname/-JCL-Spectre-i7-Vega/}
-    echo $fname2
+        fname2=${fname/-JCL-Spectre-i7-Vega/}
+        echo $fname2
 
-    if [ -e "$fname2" ]; then
+        if [ -e "$fname2" ]; then
 
-    compare -compose src "${fname}" "${fname2}" ${dname}
+            compare -compose src "${fname}" "${fname2}" ${dname}
 
-    convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:
+            convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:
 
-    if [ $(convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:) -eq 1 ]; then        
-        echo "delte"
-        rm -v "${fname}" ${dname}
-    else
-        echo "keep"
-    fi
+            if [ $(convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:) -eq 1 ]; then
+                echo "delte"
+                rm -v "${fname}" ${dname}
+            else
+                echo "keep"
+            fi
 
-    else
-        echo "not found"
-    fi
-    
+        else
+            echo "not found"
+        fi
+
+    done
 done
