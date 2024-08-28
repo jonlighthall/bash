e652205743c2dbe761228170134ae397ed903e81
Jon Lighthall <jon.lighthall@gmail.com>
2024-08-28 11:23:11 -0500

    add branch and remote to gita

--- git/gita.sh
+++ git/gita.sh
@@ -1,21 +1,42 @@
 #!/bin/bash -u
-TAB="     "
+TAB="  "
 echo "current author name:"
 git config --get user.name | uniq | sed "s/^/${TAB}/"
 echo
 echo "current author email:"
 git config --get user.email | uniq | sed "s/^/${TAB}/"
-echo
-if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-    echo "list of authors:"
-    git log --all --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+
+function print_auth() {
     echo
-    echo "list of names:"
-    git log --all --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
+    echo -e "$@"
+    if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+        (
+            echo "list of authors:"
+            git log "$@" --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+            echo
+            echo "list of names:"
+            git log "$@" --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
+            echo
+            echo "list of emails:"
+            git log "$@" --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
+        ) | sed "s/^/${TAB}/"
+    else
+        echo "$PWD is not a Git repsoitory"
+    fi
     echo
-    echo "list of emails:"
-    git log --all --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
+}
+
+print_auth --all
+
+current_branch=$(git branch | grep "^*" | sed "s/* //")
+print_auth ${current_branch}
+
+git rev-parse --abbrev-ref @{upstream} &>/dev/null
+RETVAL=$?
+if [[ $RETVAL -ne 0 ]]; then
+    do_cmd git rev-parse --abbrev-ref @{upstream}
+    echo "no remote tracking branch set for current branch"
 else
-    echo "$PWD is not a Git repsoitory"
+    remote_tracking_branch=$(git rev-parse --abbrev-ref @{upstream})
+    print_auth ${remote_tracking_branch}
 fi
-echo
