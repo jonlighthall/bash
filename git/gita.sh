bc89de63fc784323f05371cda8873b25c2c187b8
Jon Lighthall <jon.lighthall@gmail.com>
2024-08-29 09:08:04 -0500

    add color to gita

--- git/gita.sh
+++ git/gita.sh
@@ -8,28 +8,34 @@ git config --get user.email | uniq | sed "s/^/${TAB}/"
 
 function print_auth() {
     echo
+    echo "-----------------------------------------------"
     echo -e "$@"
+
+    # strip color
+    local arg
+    arg=$(echo "$@" | sed -r "s/\\\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
+
     if [ ! -z $(git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
         (
             echo "list of authors:"
-            git log "$@" --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
+            git log "$arg" --pretty=format:"%aN %aE" | sort | uniq -c | sort -n
             echo
             echo "list of names:"
-            git log "$@" --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
+            git log "$arg" --pretty=format:"%aN" | sort -u | sed "s/^/${TAB}/"
             echo
             echo "list of emails:"
-            git log "$@" --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
+            git log "$arg" --pretty=format:"%aE" | sort -u | sed "s/^/${TAB}/"
         ) | sed "s/^/${TAB}/"
     else
         echo "$PWD is not a Git repsoitory"
     fi
-    echo
+#    echo
 }
 
 print_auth --all
 
 current_branch=$(git branch | grep "^*" | sed "s/* //")
-print_auth ${current_branch}
+print_auth ${GOOD}${current_branch}${RESET}
 
 git rev-parse --abbrev-ref @{upstream} &>/dev/null
 RETVAL=$?
@@ -38,5 +44,5 @@ if [[ $RETVAL -ne 0 ]]; then
     echo "no remote tracking branch set for current branch"
 else
     remote_tracking_branch=$(git rev-parse --abbrev-ref @{upstream})
-    print_auth ${remote_tracking_branch}
+    print_auth ${ARG}${remote_tracking_branch}${RESET}
 fi
