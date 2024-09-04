165161ea2fd0dc5ecf9cc91cd8ab89a8d07fa7ef
Jon Lighthall <jon.lighthall@gmail.com>
2024-09-04 10:49:12 -0500

    add comments, move rm temp

--- onedrive/rm_onedrive_dupes.sh
+++ onedrive/rm_onedrive_dupes.sh
@@ -1,9 +1,13 @@
 # remove duplicate images
 
+# define name for temporary image difference file
 dname=diff.jpg
 
+# define OneDrive duplice suffixes
 list=( "-DTWUSC001" "-LPWUD110" "-JCL-Spectre-i7-Vega" )
+# loop over suffixes
 for suff in ${list[@]}; do
+    # loop over matching file names
     for fname in *${suff}*; do
         echo $fname
 
@@ -15,14 +19,19 @@ for suff in ${list[@]}; do
             exit 1
         fi
 
+        # strip suffix
         fname2=${fname//"${suff}"/}
         echo -n "   original file ${fname2}... "
 
+        # look for original file name
         if [ -e "$fname2" ]; then
             echo "found"
+
+            # get file type
             type="$(file ${fname})"
             echo "   $type"
 
+            # use diff for text files
             if [[ "${type}" == *"ASCII"* ]]; then
                 echo    "   text"
 
@@ -39,18 +48,24 @@ for suff in ${list[@]}; do
                 fi
             fi
 
+            # use ImageMagick for images
             if [[ "${type}" == *"image"* ]]; then
                 echo "   image"
+                # save image differences to temporary file
                 compare -compose src "${fname}" "${fname2}" ${dname}
+                # compress differences down to one pixel
                 echo -n "   diff pixel average: "
                 convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:
 
+                # if the pixel value is 1, the diff image is all black and there
+                # are no diffrences
                 if [ $(convert ${dname} -scale 1x1! -format "%[fx:u]\n" info:) -eq 1 ]; then
                     echo "   delete"
-                    rm -v "${fname}" "${dname}"
+                    rm -v "${fname}"
                 else
                     echo "   keep"
                 fi
+                rm -v "${dname}"
             fi
         else
             echo "not found"
