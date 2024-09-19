dcc2ec93c02e4fed58b97db7391d98580b107ea8
Jon Lighthall <jon.lighthall@gmail.com>
2024-09-19 11:11:58 -0500

    use python file as argument

--- git/filter/filter-repo-author.sh
+++ git/filter/filter-repo-author.sh
@@ -37,35 +37,7 @@ if [ -f ./.git-rewirte ]; then
     rm -rdv ./.git-rewrite
 fi
 
-git filter-repo $@ --partial --commit-callback '
-# define correct name
-CORRECT_NAME = b"Jon Lighthall"
-
-# define correct email
-CORRECT_EMAIL = b"jon.lighthall@gmail.com"
-
-# list emails to replace
-auth_list = [b"jonlighthall@users.noreply.github.com"]
-# define list file
-file_name = os.path.expanduser("~/utils/bash/git/filter/author_list.txt")
-# check if file exists
-if os.path.isfile(file_name):
-    # File exists
-    with open(file_name, "r", encoding="us-ascii") as file:
-        for line in file:
-            auth_list.append(line.strip().encode())
-else:
-    # File not found, print error message
-    print("Error: File not found")
-
-# conditionally replace emails and names
-if commit.author_email in auth_list:
-    # check if author email is correct (added to replace unfilter)
-    if commit.author_email != CORRECT_EMAIL:
-        commit.author_email = CORRECT_EMAIL
-    if commit.author_name != CORRECT_NAME:
-        commit.author_name = CORRECT_NAME
-'
+git filter-repo $@ --partial --commit-callback filter.py
 
 dtab
 print_done
---
+++ git/filter/filter.py
@@ -0,0 +1,36 @@
+;alsdkjf;aksjd; a;sd
+
+
+
+# define correct name
+CORRECT_NAME = b"Jon Lighthallz"
+
+# define correct email
+CORRECT_EMAIL = b"jon.lighthall@gmail.com"
+
+# list emails to replace
+auth_list = [b"jonlighthall@users.noreply.github.com"]
+# define list file
+file_name = os.path.expanduser("~/utils/bash/git/filter/author_list.txt")
+# check if file exists
+if os.path.isfile(file_name):
+    # File exists
+    with open(file_name, "r", encoding="us-ascii") as file:
+        for line in file:
+            auth_list.append(line.strip().encode())
+else:
+    # File not found, print error message
+    print("Error: File not found")
+
+# conditionally replace emails and names
+if commit.author_email in auth_list:
+    if commit.author_email != CORRECT_EMAIL:
+        commit.author_email = CORRECT_EMAIL
+    if commit.author_name != CORRECT_NAME:
+        commit.author_name = CORRECT_NAME
+
+if commit.committer_email in auth_list:
+    if commit.committer_email != CORRECT_EMAIL:
+        commit.committer_email = CORRECT_EMAIL
+    if commit.committer_name != CORRECT_NAME:
+        commit.committer_name = CORRECT_NAME
