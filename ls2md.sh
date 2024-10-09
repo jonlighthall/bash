e5151084a1697362f410c462df73e99a06541918
Jon Lighthall <jon.lighthall@gmail.com>
2024-10-09 15:03:15 -0500

    put files names in quotes for windows

--- ls2md.sh
+++ ls2md.sh
@@ -20,39 +20,39 @@ fname=ls2md.md
 echo "Output file = ${fname}"
 
 # print current directory
-echo -e "# Contents of $(pwd)\n" >${fname}
+echo -e "# Contents of $(pwd)\n" >"${fname}"
 
 # loop through each argument
 for ext in $arg; do
     echo "filtering $arg"
-    echo -e "## ${ext} files\n" >>${fname}
+    echo -e "## ${ext} files\n" >>"${fname}"
     find -L ./ -not -path "*/.git*/*" -type f -name "*${ext}" | sed "s,^./,,;s/${ext}$//" | sort | sed "s,^.*$,[\`&\`](&${ext}),;$ ! s,$, \\\\," >>${fname}
 done
 
 # save root directories to file
-echo -e "\n## Directories\n" >>${fname}
+echo -e "\n## Directories\n" >>"${fname}"
 find -L ./ -not -path "*/.git*" -not -path "*/.vscode*" -type d | sed 's,^./,,;/\//d;/^$/d' | sort | sed "s,^.*$,[\`&\`](&),;$ ! s,$, \\\\," >>${fname}
 
 echo "done"
 
 # print contents of file
 echo "Contents of ${fname}:"
-cat ${fname} | sed "s/^/   /"
+cat "${fname}" | sed "s/^/   /"
 
 # define ignore file
 echo
 ignore_file=$(pwd)/.gitignore
 echo "Checking ${ignore_file}..."
-if [ -f ${ignore_file} ]; then
-    if grep -q ${fname} ${ignore_file}; then
+if [ -f "${ignore_file}" ]; then
+    if grep -q "${fname}" "${ignore_file}"; then
         echo "${fname} already in ${ignore_file}"
     else
         echo "Adding ${fname} to ${ignore_file}..."
-        echo -e "\n#Markdown list" >>${ignore_file}
-        echo ${fname} >>${ignore_file}
+        echo -e "\n#Markdown list" >>"${ignore_file}"
+        echo "${fname}" >>"${ignore_file}"
     fi
 else
     echo "Creating ${ignore_file}..."
-    echo -e "#Markdown list" >${ignore_file}
-    echo ${fname} >>${ignore_file}
+    echo -e "#Markdown list" >"${ignore_file}"
+    echo "${fname}" >>"${ignore_file}"
 fi
