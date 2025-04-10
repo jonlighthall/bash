7cbf215ceddc138739250c778b65222cea57bd93
Jon Lighthall <jonathan.c.lighthall.civ@us.navy.mil>
2025-04-10 10:07:54 -0500

    get_wttr: change escape character

--- test/get_wttr.sh
+++ test/get_wttr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -u
-curl wttr.in/30.4278,-90.0911?u1 2>/dev/null | sed 's/^/\E[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/'
+curl wttr.in/30.4278,-90.0911?u1 2>/dev/null | sed 's/^/\x1B[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/'
 echo -e "\E[0m"
