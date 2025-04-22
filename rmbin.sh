083d10ff4c39b73c305b0ef32d54011f03eda664
Jon Lighthall <jon.lighthall@gmail.com>
2025-04-22 09:03:25 -0500

    remove first nested if

--- rmbin.sh
+++ rmbin.sh
@@ -17,54 +17,54 @@ fi
 if [ $# -eq 0 ]; then
 	  echo "Please provide a target directory"
 	  exit 1
-else
-	  if [[ -d $1 ]]; then
-		    echo "found $1"
+fi
 
-		    cd $1
-		    echo $PWD
+if [[ -d $1 ]]; then
+		echo "found $1"
 
-		    if git rev-parse --git-dir &>/dev/null; then
-			      # This is a valid git repository
-			      echo "$1 is part of a Git repository"
-			      GITDIR=$(git rev-parse --git-dir)
-			      echo "the .git folder is $GITDIR"
+		cd $1
+		echo $PWD
 
-			      # first, remove tracked files from the repository
-			      echo "removing tracked binary (and empty) files from the repository..."
-			      find ./ -type f -not -path "*$GITDIR/*" -not -path "*/.git/*" | perl -lne 'print if -B' | xargs -r git rm --ignore-unmatch
+		if git rev-parse --git-dir &>/dev/null; then
+			  # This is a valid git repository
+			  echo "$1 is part of a Git repository"
+			  GITDIR=$(git rev-parse --git-dir)
+			  echo "the .git folder is $GITDIR"
 
-			      # then, remove remaining binary files
-			      echo "removing untracked binary files..."
-			      find ./ -type f -not -path "*$GITDIR/*" -not -path "*/.git/*" | perl -lne 'print if -B' | xargs -r rm -v
-		    else
-			      # this is not a git repository
-			      echo "$1 is not part of a Git repsoity"
-			      echo "removing binary files..."
-            itab
-            for file in $(find -L ./ -type f -not -path "*/.git/*"); do
-                # Check if the file is binary
-                if perl -e 'exit -B $ARGV[0]' "$file"; then
-                    :  #echo "File is text."; file $file
+			  # first, remove tracked files from the repository
+			  echo "removing tracked binary (and empty) files from the repository..."
+			  find ./ -type f -not -path "*$GITDIR/*" -not -path "*/.git/*" | perl -lne 'print if -B' | xargs -r git rm --ignore-unmatch
+
+			  # then, remove remaining binary files
+			  echo "removing untracked binary files..."
+			  find ./ -type f -not -path "*$GITDIR/*" -not -path "*/.git/*" | perl -lne 'print if -B' | xargs -r rm -v
+		else
+			  # this is not a git repository
+			  echo "$1 is not part of a Git repsoity"
+			  echo "removing binary files..."
+        itab
+        for file in $(find -L ./ -type f -not -path "*/.git/*"); do
+            # Check if the file is binary
+            if perl -e 'exit -B $ARGV[0]' "$file"; then
+                :  #echo "File is text."; file $file
+            else
+                if [ -s $file ]; then
+                    echo -e "${TAB}${BAD}${file##*./}${RESET} is binary"
                 else
-                    if [ -s $file ]; then
-                        echo -e "${TAB}${BAD}${file##*./}${RESET} is binary"
-                    else
-                        echo -e "${TAB}${YELLOW}${file##*./}${RESET} is empty"
-                    fi
-                    itab
-                    echo -en "${TAB}"
-                    ls $file -l --color=always
-                    echo -en "${TAB}"
-                    rm -rv $file
-                    dtab
+                    echo -e "${TAB}${YELLOW}${file##*./}${RESET} is empty"
                 fi
-            done
-            dtab
-		    fi
-	  else
-		    echo "$1 is not found"
-		    exit 1
-	  fi
+                itab
+                echo -en "${TAB}"
+                ls $file -l --color=always
+                echo -en "${TAB}"
+                rm -rv $file
+                dtab
+            fi
+        done
+        dtab
+		fi
+else
+		echo "$1 is not found"
+		exit 1
 fi
 set_exit
