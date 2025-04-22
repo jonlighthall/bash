e2e0fb8b46c2f503f019d16a7d1068eaf68fbec5
Jon Lighthall <jon.lighthall@gmail.com>
2025-04-22 09:29:06 -0500

    add less

--- test/get_wttr.sh
+++ test/get_wttr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -u
 timeout -s 9 1s \
         curl wttr.in/30.4278,-90.0911?u1 2>/dev/null \
-    | sed 's/^/\x1B[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/'
+    | sed 's/^/\x1B[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/' \
+    | less -ESX
 echo -e "\E[0m"
