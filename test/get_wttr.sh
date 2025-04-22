9277010b46430f2e7f6b598f86f5b99c1a1f2507
Jon Lighthall <jon.lighthall@gmail.com>
2025-04-22 09:22:26 -0500

    get_wttr: add timeout, add line continuation

--- test/get_wttr.sh
+++ test/get_wttr.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -u
-curl wttr.in/30.4278,-90.0911?u1 2>/dev/null | sed 's/^/\x1B[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/'
+timeout -s 9 1s \
+        curl wttr.in/30.4278,-90.0911?u1 2>/dev/null \
+    | sed 's/^/\x1B[40;37m/;s/\E\[0m/\E[40;37m/g;s/46/0;46/'
 echo -e "\E[0m"
