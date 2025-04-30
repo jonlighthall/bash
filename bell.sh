39d3b7a4e493f3f3ade64b8e11b644c1adf6da5f
Jon Lighthall <jon.lighthall@gmail.com>
2025-04-30 11:21:12 -0500

    increase wait time; tested on count

--- bell.sh
+++ bell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -u
 for i in {1..3}; do
     echo -e "$i beeps \a"
-    sleep 1
+    sleep 1.25
 done
 # print time at exit
 echo -e "\n$(date +"%a %b %-d %-l:%M %p %Z") ${BASH_SOURCE##*/} $(sec2elap $SECONDS)"
