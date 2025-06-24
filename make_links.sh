c4cea55c9c0abf099e53dba32d380ee205ddf023
Jon Lighthall <jon.lighthall@gmail.com>
2025-06-24 16:57:41 -0500

    minor changes to match config/make links

--- make_links.sh
+++ make_links.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -u
+# ------------------------------------------------------------------------------
 #
 # Jun 2020 JCL
+#
+# ------------------------------------------------------------------------------
 
 # get starting time in nanoseconds
 start_time=$(date +%s%N)
 
 # load bash utilities
-fpretty="${HOME}/config/.bashrc_pretty"
+config_dir="${HOME}/config"
+fpretty="${config_dir}/.bashrc_pretty"
 if [ -e "$fpretty" ]; then
     source "$fpretty"
     set_traps
+    print_source
 fi
 
 # determine if script is being sourced or executed
@@ -17,7 +22,9 @@ if ! (return 0 2>/dev/null); then
     # exit on errors
     set -e
 fi
-print_source
+
+# it is assumed that the fisrt command to be run after cloning the parent
+#   repository is make_links.sh (this file)
 
 # save and print starting directory
 start_dir=$PWD
@@ -28,7 +35,6 @@ proj_name=$(basename "${src_dir_phys}")
 echo " project directory = $proj_name"
 target_dir="${HOME}/utils/${proj_name}"
 link_dir=$HOME/bin
-
 do_make_link "${link_dir}"
 
 cbar "Start Linking Repo Files"
