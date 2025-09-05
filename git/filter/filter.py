ae94f3f9fbca344cae4c11cd41cee92c2f993d3d
Jon Lighthall <jon.lighthall@gmail.com>
2025-09-05 14:20:00 -0500

    add exit on no author file, add print on change

--- git/filter/filter.py
+++ git/filter/filter.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Note: This is a git-filter-repo callback script.
+# The 'commit' object is automatically provided by git-filter-repo
+# when this script is executed as a callback.
+
 # define correct name
 CORRECT_NAME = b"Jon Lighthall"
 
@@ -15,18 +22,43 @@ if os.path.isfile(file_name):
         for line in file:
             auth_list.append(line.strip().encode())
 else:
-    # File not found, print error message
-    print("Error: File not found")
+    # File not found, print error message and exit
+    print(f"Error: Author list file not found: {file_name}")
+    sys.exit(1)
 
 # conditionally replace emails and names
+commit_header_printed = False
+
 if commit.author_email in auth_list:
+    commit_hash = (
+        commit.original_id.decode() if commit.original_id else f"mark:{commit.id}"
+    )
+    print(f"Commit {commit_hash[:8]}:")
+    commit_header_printed = True
     if commit.author_email != CORRECT_EMAIL:
+        print(
+            f"   Updating    author: {commit.author_email.decode()} -> {CORRECT_EMAIL.decode()}"
+        )
         commit.author_email = CORRECT_EMAIL
     if commit.author_name != CORRECT_NAME:
+        print(
+            f"   Updating    author: {commit.author_name.decode()} -> {CORRECT_NAME.decode()}"
+        )
         commit.author_name = CORRECT_NAME
 
 if commit.committer_email in auth_list:
+    commit_hash = (
+        commit.original_id.decode() if commit.original_id else f"mark:{commit.id}"
+    )
+    if not commit_header_printed:
+        print(f"Commit {commit_hash[:8]}:")
     if commit.committer_email != CORRECT_EMAIL:
+        print(
+            f"   Updating committer: {commit.committer_email.decode()} -> {CORRECT_EMAIL.decode()}"
+        )
         commit.committer_email = CORRECT_EMAIL
     if commit.committer_name != CORRECT_NAME:
+        print(
+            f"   Updating committer: {commit.committer_name.decode()} -> {CORRECT_NAME.decode()}"
+        )
         commit.committer_name = CORRECT_NAME
